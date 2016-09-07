561dddf5f627d3c24e89e3bd3973853bb57e53e3
dkelz16 <dkeller16x@yahoo.com>
2016-09-07 15:47:33 -0400

    Added Folder "Dave"

---
+++ dave/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<title> 
+			Dave&apos;s Web Page
+		</title>
+	</head>
+	
+	<body>
+		<h1>My Page On The Internet</h1>
+		<?= date("Y"); ?>
+	</body>
+
+</html>
