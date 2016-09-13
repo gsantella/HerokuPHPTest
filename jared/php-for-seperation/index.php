9ff5ab339ac28ccd137b8fa4cd05c7d2079ce387
jsmith129540 <jsmith75@southhills.edu>
2016-09-13 14:08:52 -0400

    updated header

--- jared/php-for-seperation/index.php
+++ jared/php-for-seperation/index.php
@@ -10,7 +10,7 @@
 <body>
 	<header>
 		
-		<p>this is the header for all pages</p>
+		<p>this is the header for all pages!</p>
 		<a href="index.php" >Home</a>
 		<a href="page1.php" >Page 1</a>
 		<a href="page2.php" >Page 2</a>
