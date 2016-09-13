8c7079eb34cc5e6c0c8b596181cfbed81ce97d90
gsantella <gsantella@southhills.edu>
2016-09-13 14:08:31 -0400

    updated header stupidly

--- guido/php-for-separation/page1.php
+++ guido/php-for-separation/page1.php
@@ -8,7 +8,7 @@
 
 <body>
 	<header>
-		<p>This is the header content for all pages</p>
+		<p>This is the header content for all pages!</p>
 		<a href="index.php">Home</a>
 		<a href="page1.php">Page1</a>
 		<a href="page2.php">Page2</a>
