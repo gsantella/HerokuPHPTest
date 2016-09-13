665777e2fc68d0c64003421aad6fc75d100bde8d
diddymitchell23 <diddymitchell23@aol.com>
2016-09-13 14:31:23 -0400

    Final Update FooterHeader

--- preston/php-for-seperation/index.php
+++ preston/php-for-seperation/index.php
@@ -10,12 +10,7 @@
 <body>
 
 	<?php include("includes/header.php"); ?>
-	<header>
-		<p>This is the header content for all pages!</p>
-		<a href="index.php">Home</a>
-		<a href="page1.php">Page 1</a>
-		<a href="page2.php">Page 2</a>
-	</header>
+
 	
 	<main>
 		<p> This is the main context of index</p>
