acdeacc80308fe53428f50d23bf568ec40a4a97d
jsmith129540 <jsmith75@southhills.edu>
2016-09-13 14:23:28 -0400

    added to the header and footer

--- jared/php-for-seperation/includes/footer.php
+++ jared/php-for-seperation/includes/footer.php
@@ -2,5 +2,6 @@
 <footer>
 	
 	<p>this is the footer for all pages</p>
+	<p>can i see it</p>
 	
 </footer>
--- jared/php-for-seperation/includes/header.php
+++ jared/php-for-seperation/includes/header.php
@@ -1,6 +1,7 @@
 <header>
 	
 	<p>this is the header for all pages!</p>
+	<p>Hello There!</p>
 	<a href="index.php" >Home</a>
 	<a href="page1.php" >Page 1</a>
 	<a href="page2.php" >Page 2</a>
