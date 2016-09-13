199a1267071a8353cbf4ed0e1f0b836244e32451
gsantella <gsantella@southhills.edu>
2016-09-13 14:18:51 -0400

    separate header and footer with includes

--- guido/php-for-separation/index.php
+++ guido/php-for-separation/index.php
@@ -7,18 +7,15 @@
 </head>
 
 <body>
-	<header>
-		<p>This is the header content for all pages!</p>
-		<a href="index.php">Home</a>
-		<a href="page1.php">Page1</a>
-		<a href="page2.php">Page2</a>
-	</header>
+
+	<?php include("includes/header.php"); ?>
+	
 	<main>
 		<p>This is the main content of index.php</p>
 	</main>
-	<footer>
-		<p>This is the footer content for all pages</p>
-	</footer>
+	
+	<?php include("includes/footer.php"); ?>
+	
 </body>
 
 </html>
