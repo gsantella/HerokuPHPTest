9d0cab37304ddb78f3b308b69a95089cb558a705
ckimberly97 <ckimberly97@southhills.edu>
2016-09-13 14:24:49 -0400

    Update header and Footer

--- colton/php-for-seperation/includes/header.php
+++ colton/php-for-seperation/includes/header.php
@@ -1,5 +1,5 @@
 <header>
-<p>This is the header content for all pages!</p>
+<p>This is the header content for all pages!!!!!!!</p>
 <a href="index.php">home</a>
 <a href="page1.php">home</a>
 <a href="page2.php">home</a>
