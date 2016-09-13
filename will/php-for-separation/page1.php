30a0318d7d174909a24a5126d3464f50224b0e65
ghill714 <wosemwegie88@southhills.edu>
2016-09-13 12:16:24 -0400

    added pages to separation

---
+++ will/php-for-separation/page1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Index Page</title>
+	<link rel="icon" type="image/png" href="favicon.png" />
+</head>
+<body>
+	<header>
+		<P>This is the header content for all pages</p>
+		<a href="index.php">Home / </a>
+		<a href="page1.php">Page1 / </a>
+		<a href="page2.php">Page2 / </a>
+	</header>
+	<main>
+		<p>This is the main content of index.php</p>
+	</main>
+	<footer>
+		<p>This is the footer content for all pages</p>
+	</footer>
+</body>
+</html>
