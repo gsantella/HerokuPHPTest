cf0c9bad1b466531c6376e62a9046cad423c660e
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-13 14:08:35 -0400

    updated Header stupidly

--- allen/php-for-seperation/includes/footer.php
+++ allen/php-for-seperation/includes/footer.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Index Page</title>
-		<link rel="icon" type="image/png" href="favicon.png" />
-	</head>
-	
-	<body>
-		<header>
-			<p>This is the header content for all pages</p>
-			<a href="index.php">Home</a>
-			<a href="page1.php">Page1</a>
-			<a href="page2.php">Page2</a>
-		</header>
-		<main>
-			<p>This is the main context of index.php</p>
-		</main>
-		<footer>
-			<p>This is the footer content for all pages</p>
-		</footer>
-	</body>
-</html>
+<footer>
+	<p>This is the footer content for all pages</p>
+</footer>
