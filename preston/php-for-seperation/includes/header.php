83243f4a1de4d07676e609b8d031c63eb9469e88
diddymitchell23 <diddymitchell23@aol.com>
2016-09-13 14:26:54 -0400

    added to header

--- preston/php-for-seperation/includes/header.php
+++ preston/php-for-seperation/includes/header.php
@@ -1,5 +1,5 @@
 	<header>
-		<p>This is the header content for all pages!</p>
+		<p>This is the header content for all pages!!!!</p>
 		<a href="index.php">Home</a>
 		<a href="page1.php">Page 1</a>
 		<a href="page2.php">Page 2</a>
