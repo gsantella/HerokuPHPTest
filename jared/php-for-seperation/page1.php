2bc904cb3f9564325f552a76f886036b1d4f2612
jsmith129540 <jsmith75@southhills.edu>
2016-09-13 12:15:41 -0400

    pages added

---
+++ jared/php-for-seperation/page1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Index Page</title>
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+</head>
+
+
+<body>
+	<header>
+		
+		<p>this is the header for all pages</p>
+		<a href="index.php" ></a>
+		<a href="page1.php" ></a>
+		<a href="page2.php" ></a>
+		
+	</header>
+	<main>
+		
+		<p>this is the main content of page2.php</p>
+		
+	</main>
+	<footer>
+		
+		<p>this is the footer for all pages</p>
+		
+	</footer>
+</body>
+
+
+</html>
