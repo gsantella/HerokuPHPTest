1cc8bbcaf4971ce0f4453bdd075d0c750b5428b0
DarkFox216 <Darkfox216.@gmail.com>
2016-09-13 14:08:38 -0400

    Updated Header (!)

--- marcus/php-for-separation/Page2.php
+++ marcus/php-for-separation/Page2.php
@@ -8,7 +8,7 @@
 
 <body>
 	<header>
-		<p> This is the header content for all pages</p>
+		<p> This is the header content for all pages!</p>
 		<a href="index.php">Home</a>
 		<a href="Page1.php">Page1</a>
 		<a href="Page2.php">Page2</a>
