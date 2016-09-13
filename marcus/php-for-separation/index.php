3fb420c92a1849198f8c61cb85b33adade7dd8af
DarkFox216 <Darkfox216.@gmail.com>
2016-09-13 12:16:47 -0400

    Added Pages 1,2,index

---
+++ marcus/php-for-separation/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> </title>
+	<link rel="icon" type="image/png" href="favicon.png" />
+</head>
+
+<body>
+	<header>
+		<p> This is the header content for all pages</p>
+		<a href="index.php">Home</a>
+		<a href="Page1.php">Page1</a>
+		<a href="Page2.php">Page2</a>
+	</header>
+	<main>
+		<p> This is the main content for Index </p>
+	<main/>
+	<footer>
+		<p> This is the footer content for all pages</p>
+	</footer>
+</body>
+
+</html>
