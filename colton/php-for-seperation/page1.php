38effb1c4a7db3cb5099b866ba09a28f55a9fae2
ckimberly97 <ckimberly97@southhills.edu>
2016-09-13 12:15:42 -0400

    Pages added

---
+++ colton/php-for-seperation/page1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html> 
+
+<head> 
+	<title>Index Page</title>
+	<link rel="icon" type="image/png" href="favicon.png" />
+
+</head>
+
+<body>
+	<header>
+		<p>This is the header content for all pages</p>
+		<a href="index.php">home</a>
+		<a href="page1.php">home</a>
+		<a href="page2.php">home</a>
+	</header>
+	<main>
+		<p>This is the main content of Page 1</p>
+	</main>
+	<footer>
+		<p>This is the footer content for all pages</p>
+	</footer>
+</body>
+
+</html>
