c2a853a5058b2ebd940d031f1278ab8761d9d229
dkelz16 <dkeller16x@yahoo.com>
2016-09-13 12:15:35 -0400

    pages added

---
+++ dave/php-for-separation/page1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> 
+		Index Page
+	</title>
+	<link rel="icon" type="image\png" href="favicon.png"/>
+</head>
+
+<body>
+	<header>
+		<p>
+		This is the header content for all pages.
+		</p>
+		<a href="index.php"> Home Page </a>
+		<a href="page1.php"> Page1 </a>
+		<a href="page2.php"> Page2 </a>
+	</header>
+	
+	<main>
+		<p>
+		This is the main content of page1.php.
+		</p>
+	</main>
+
+	
+	<footer>
+		<p>
+		This is the footer content for all pages.
+		</p>
+</footer>
+</body>
+
+
+
+</html>
