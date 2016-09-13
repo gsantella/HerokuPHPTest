6ccb73eff21310c7be64556f399754150c121d74
dkelz16 <dkeller16x@yahoo.com>
2016-09-13 14:10:19 -0400

    fixed space

--- dave/php-for-separation/index.php
+++ dave/php-for-separation/index.php
@@ -13,7 +13,7 @@
 		<p>
 		This is the header content for all pages!
 		</p>
-		<a href="index.php"> Home Page </a>
+		<a href="index.php"> Home </a>
 		<a href="page1.php"> Page1 </a>
 		<a href="page2.php"> Page2 </a>
 	</header>
