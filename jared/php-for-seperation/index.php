bc16eabfaac9f89302620c2bf6062f2780167e79
jsmith129540 <jsmith75@southhills.edu>
2016-09-13 12:18:50 -0400

    fixed wording bug

--- jared/php-for-seperation/index.php
+++ jared/php-for-seperation/index.php
@@ -11,9 +11,9 @@
 	<header>
 		
 		<p>this is the header for all pages</p>
-		<a href="index.php" ></a>
-		<a href="page1.php" ></a>
-		<a href="page2.php" ></a>
+		<a href="index.php" >Home</a>
+		<a href="page1.php" >Page 1</a>
+		<a href="page2.php" >Page 2</a>
 		
 	</header>
 	<main>
--- jared/php-for-seperation/page1.php
+++ jared/php-for-seperation/page1.php
@@ -11,14 +11,14 @@
 	<header>
 		
 		<p>this is the header for all pages</p>
-		<a href="index.php" ></a>
-		<a href="page1.php" ></a>
-		<a href="page2.php" ></a>
+		<a href="index.php" >Home</a>
+		<a href="page1.php" >Page 1</a>
+		<a href="page2.php" >Page 2</a>
 		
 	</header>
 	<main>
 		
-		<p>this is the main content of page2.php</p>
+		<p>this is the main content of page1.php</p>
 		
 	</main>
 	<footer>
