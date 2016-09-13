90118e673c64c7e7f3b026fe4d751a6a9389bca5
ghill714 <wosemwegie88@southhills.edu>
2016-09-13 14:25:52 -0400

    checked includes php

--- will/php-for-separation/includes/header.php
+++ will/php-for-separation/includes/header.php
@@ -1,5 +1,5 @@
 <header>
-	<P>This is the header content for all pages!</p>
+	<P>This is the header content for all pages!!!!!!!!!!</p>
 	<a href="index.php">Home / </a>
 	<a href="page1.php">Page1 / </a>
 	<a href="page2.php">Page2 / </a>
