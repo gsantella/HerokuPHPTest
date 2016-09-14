4f55372046502432c31c97bf3193321150406bcb
jsmith129540 <jsmith75@southhills.edu>
2016-09-14 15:50:55 -0400

    Fixed missing brackets

--- jared/basic-db-access/index.php
+++ jared/basic-db-access/index.php
@@ -22,7 +22,7 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 	</head>
 
 	<body>
-		<?php print_r$result; ?>
+		<?php print_r($result); ?>
 	</body>
 	
 </html>
