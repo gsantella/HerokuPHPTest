7d14cf59a90c55230dc30aa01012a77aeb47ad5e
ckimberly97 <ckimberly97@southhills.edu>
2016-09-14 15:30:42 -0400

    Test Database code ????ck

---
+++ colton/basic-db-access/index.php
@@ -0,0 +1,28 @@
+<?php
+//Create a connection by logging in and selecting database(login)
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;username=$user;password=$pass";
+$database = new PDO($dsn);
+
+//Run a SELECT SQL Statement to get results
+$result = $database->query("SELECT * FROM students;")->fetchALL(PDO::FETCH_ASSOC);
+
+//Close connection (logout)
+//Ignore for now 
+?>
+<!DOCTYPE html>
+<html>
+
+<head> 
+	<title>Basic Database Access</title>
+</head>
+
+<body>
+		<?php echo $result; ?>
+</body>
+
+</html>
