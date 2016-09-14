515770a5ade33d6fcc3b4149c9484ab3feb8a5ee
ghill714 <wosemwegie88@southhills.edu>
2016-09-14 15:32:49 -0400

    test database code

---
+++ will/basic-db-access/index.php
@@ -0,0 +1,28 @@
+<?php 
+//Create connection by logging in and selecting database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;username=$user;password=$pass";
+$database = new PDO($dsn);
+
+//Run a SELECT SQL Statement to get results
+$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+
+//Close connection 
+//Ignore for now
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Basic Database Access</title>
+</head>
+
+<body>
+	<?php echo $result; ?>
+</body>
+
+</html>
