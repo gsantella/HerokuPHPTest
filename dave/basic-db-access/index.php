6abad52101d5ef01d4f714f2caa6f096fe478611
dkelz16 <dkeller16x@yahoo.com>
2016-09-14 15:44:59 -0400

    bug fixed

--- dave/basic-db-access/index.php
+++ dave/basic-db-access/index.php
@@ -5,12 +5,12 @@ $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
 $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
 
-$dsn = "pgsql:dbname=$db;host=$host;username:user;password:pass";
+$dsn = "pgsql:dbname=$db;host=$host;user:user;password:pass";
 $database = new PDO($dsn);
 
 
 //Run a SELECT SQL Statement To Get Results
-$result = $database->query("SELECT * FROM students;");
+$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
 
 
 
