625385d504b47cdf1d0501bb40d9337ef6fc11e4
ghill714 <wosemwegie88@southhills.edu>
2016-09-14 15:48:29 -0400

    updated basic db access index file

--- will/basic-db-access/index.php
+++ will/basic-db-access/index.php
@@ -5,7 +5,7 @@ $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
 $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
 
-$dsn = "pgsql:dbname=$db;host=$host;username=$user;password=$pass";
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 
 //Run a SELECT SQL Statement to get results
@@ -22,7 +22,7 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 </head>
 
 <body>
-	<?php echo $result; ?>
+	<?php print_r($result); ?>
 </body>
 
 </html>
