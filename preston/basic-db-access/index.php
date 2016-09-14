2399ab6fbced5d6c09de33a2cc53251a263b942a
diddymitchell23 <diddymitchell23@aol.com>
2016-09-14 15:46:01 -0400

    fixed code for first database

--- preston/basic-db-access/index.php
+++ preston/basic-db-access/index.php
@@ -1,11 +1,12 @@
 <?php
+//swassssss
 //Create connection by logging in and selecting database 
 $host= "ec2-23-21-234-201.compute-1.amazonaws.com";
 $db= "ddf1ndt04b3tti";
 $user= "fpkkqmtaywxfwm";
 $pass= "dbEE8plG9zaS_QCcVzZeD7maqU";
 
-$dsn = "pgsql:dbname=$db;host=$host;username=$user;password=$pass";
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 
 //Run a SELECT SQL STATEMENT
@@ -28,6 +29,5 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 	<?php echo $result; ?>
 
 </body>
-
 
 </html>
