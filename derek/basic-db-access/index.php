53ac02ab3b86b49f17d5a7cf0fc5b39fd53ab90d
dcarey50 <dcarey50@southhills.edu>
2016-09-15 11:41:55 -0400

    added PHP table

--- derek/basic-db-access/index.php
+++ derek/basic-db-access/index.php
@@ -6,6 +6,16 @@
 </head>
 <body>
 	<?=($result[0]["first_name"]); ?>
+	
+	<table>
+	<?php foreach($result as $row): ?>
+	<tr>
+		<td><?= $row["id"]; ?></td>
+		<td><?= $row["first_name"]; ?></td>
+	</tr>
+	<?php endforeach ; ?>
+	</table>
+	
 	<table>
 		<tr><td>1</td><td>Ford</td><td>Focus</td></tr>
 		<tr><td>2</td><td>Ford</td><td>Fusion</td></tr>
