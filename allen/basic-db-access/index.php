25d3b40c918335a6eb042809573f50ed3bf8e564
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-15 11:26:47 -0400

    added manual table

--- allen/basic-db-access/index.php
+++ allen/basic-db-access/index.php
@@ -6,6 +6,46 @@
 	</head>
 	
 	<body>
-		<?= $result[0]["first_name"]; ?>
+		
+		<table>
+		
+			<tr>
+				<td>1</td>
+				<td>Ford</td>
+				<td>Focus</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Ford</td>
+				<td>Fusion</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Ford</td>
+				<td>F-150</td>
+			</tr>
+		
+			<!--
+			<th>Student</th>
+			
+			<tr>
+				<td>
+					<?= $result[0]["first_name"]; ?>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<?= $result[1]["first_name"]; ?>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<?= $result[2]["first_name"]; ?>
+				</td>
+			</tr>
+			-->
+		</table>
 	</body>
 </html>
