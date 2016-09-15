dfcad218ab7af8d2d91ab6fa6a2d34a5691cfbc2
gsantella <gsantella@southhills.edu>
2016-09-15 11:26:29 -0400

    Added demo table

--- guido/basic-db-access/index.php
+++ guido/basic-db-access/index.php
@@ -8,6 +8,24 @@
 
 <body>
 	<?= $result[0]["first_name"]; ?>
+	
+	<table>
+		<tr>
+			<td>1</td>
+			<td>Ford</td>
+			<td>Focus</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Ford</td>
+			<td>Fusion</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Ford</td>
+			<td>F150</td>
+		</tr>
+	</table>
 </body>
 
 </html>
