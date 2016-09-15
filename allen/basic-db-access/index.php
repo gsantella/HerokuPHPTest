0b8d2109427cf270b667ed4dbaaa92c052235f37
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-15 12:02:46 -0400

    added css formatting

--- allen/basic-db-access/index.php
+++ allen/basic-db-access/index.php
@@ -8,33 +8,22 @@
 	
 	<body>
 		
-		<table>
-			<?php foreach($result as $row): ?>
+		<table class="table-fill">
+			<thead>
 				<tr>
-					<td><?= $row["id"]; ?></td>
-					<td><?= $row["first_name"]; ?></td>
+					<th class="text-left">Id</th>
+					<th class="text-left">First Name</th>
 				</tr>
-			<?php endforeach; ?>
-		</table>
-		
-		<p>The number of rows in the table: <?= $result->rowCount(); ?></p>
-		
-		<table>
-			<tr>
-				<td>1</td>
-				<td>Ford</td>
-				<td>Focus</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>Ford</td>
-				<td>Fusion</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>Ford</td>
-				<td>F-150</td>
-			</tr>
+			</thead>
+			
+			<tbody class="table-hover">
+				<?php foreach($result as $row): ?>
+					<tr>
+						<td class="text-left"><?= $row["id"]; ?></td>
+						<td class="text-left"><?= $row["first_name"]; ?></td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
 		</table>
 		
 		
