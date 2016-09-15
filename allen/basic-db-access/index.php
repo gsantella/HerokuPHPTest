17025d62e3e13006eb3d14b7cc4210fc9a2ec0ad
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-15 11:13:11 -0400

    show first result

--- allen/basic-db-access/index.php
+++ allen/basic-db-access/index.php
@@ -1,21 +1,4 @@
-<?php
-//Create connection by loggin in and selecting database
-$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
-$db = "ddf1ndt04b3tti";
-$user = "fpkkqmtaywxfwm";
-$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-
-$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
-	    //POST Gress
-$database = new PDO($dsn);
-
-//Run a SELECT SQL statement to get results
-$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
-
-//Close connection
-//IGNORE FOR NOW
-
-?>
+<?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -23,6 +6,6 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 	</head>
 	
 	<body>
-		<?php print_r($row["first_name"]); ?>
+		<?php print_r($result[0]["first_name"]); ?>
 	</body>
 </html>
