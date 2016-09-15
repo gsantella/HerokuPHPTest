24c757ade29de8daedafee068959cf3babc6cd5e
jsmith129540 <jsmith75@southhills.edu>
2016-09-15 11:44:25 -0400

    added php dynamic table

--- jared/basic-db-access/index.php
+++ jared/basic-db-access/index.php
@@ -8,7 +8,14 @@
 
 	<body>
 		<?= $result[0]["first_name"]; ?>
-		
+		<table>
+		<?php foreach($database->$result as $row): ?>
+			<tr>
+				<td><?= $row["id"]; ?></td>
+				<td><?= $row["first_name"]; ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</table>
 		<table>
 			<tr>
 				<td>1</td>
@@ -18,12 +25,12 @@
 			<tr>
 				<td>1</td>
 				<td>ford<td>
-				<td>focus</td>
+				<td>f 150</td>
 			</tr>
 			<tr>
 				<td>1</td>
 				<td>ford<td>
-				<td>focus</td>
+				<td>fusion</td>
 			</tr>
 		</table>
 	</body>
