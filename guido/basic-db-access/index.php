ee21286909f5ade800aab2e69456a6878d9e4b63
gsantella <gsantella@southhills.edu>
2016-09-15 11:44:21 -0400

    Added PHP dynamic table

--- guido/basic-db-access/index.php
+++ guido/basic-db-access/index.php
@@ -9,6 +9,17 @@
 <body>
 	<?= $result[0]["first_name"]; ?>
 	
+	
+	<table>
+	<?php foreach($result as $row): ?>
+		<tr>
+			<td><?= $row["id"]; ?></td>
+			<td><?= $row["first_name"]; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</table>
+	
+	
 	<table>
 		<tr>
 			<td>1</td>
