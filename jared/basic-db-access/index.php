acde16b657a92f37eb6b5d0cd422352684901e36
jsmith129540 <jsmith75@southhills.edu>
2016-09-15 11:14:17 -0400

    added includes

--- jared/basic-db-access/index.php
+++ jared/basic-db-access/index.php
@@ -1,19 +1,4 @@
-<?php 
-// create connection by logging in and selecting database
-$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
-$db = "ddf1ndt04b3tti";
-$user = "fpkkqmtaywxfwm";
-$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-
-$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
-
-$database = new PDO($dsn);
-// run a select statement to get results
-$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
-
-//close connection
-// ignore for now
-?>
+<?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
 
@@ -22,7 +7,7 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 	</head>
 
 	<body>
-		<?php print_r($result); ?>
+		<?= $result[0]["first_name"]); ?>
 	</body>
 	
 </html>
