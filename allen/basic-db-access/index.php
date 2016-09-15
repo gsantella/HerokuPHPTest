12d2734a9c61d9465c46fe4ce210811bbb524eb4
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-15 11:40:50 -0400

    Added dynamic table

--- allen/basic-db-access/index.php
+++ allen/basic-db-access/index.php
@@ -8,7 +8,15 @@
 	<body>
 		
 		<table>
+			<?php foreach($result as $row): ?>
+				<tr>
+					<td><?= $row["id"]; ?></td>
+					<td><?= $row["first_name"]; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
 		
+		<table>
 			<tr>
 				<td>1</td>
 				<td>Ford</td>
@@ -24,28 +32,8 @@
 				<td>Ford</td>
 				<td>F-150</td>
 			</tr>
-		
-			<!--
-			<th>Student</th>
-			
-			<tr>
-				<td>
-					<?= $result[0]["first_name"]; ?>
-				</td>
-			</tr>
-			
-			<tr>
-				<td>
-					<?= $result[1]["first_name"]; ?>
-				</td>
-			</tr>
-			
-			<tr>
-				<td>
-					<?= $result[2]["first_name"]; ?>
-				</td>
-			</tr>
-			-->
 		</table>
+		
+		
 	</body>
 </html>
