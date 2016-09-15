b73d2cf5ed0f0e8429fd63541cc11cb002744035
dkelz16 <dkeller16x@yahoo.com>
2016-09-15 11:26:28 -0400

    added demo table

--- dave/basic-db-access/index.php
+++ dave/basic-db-access/index.php
@@ -8,6 +8,29 @@
 
 <body>
 	<?= $result[0]["first_name"]; ?>
+	
+	<table>
+	
+		<tr>
+			<td> 1 </td>
+			<td> Ford </td>
+			<td> Focus </td>
+		</tr>
+		
+		<tr>
+			<td> 2 </td>
+			<td> Ford </td>
+			<td> Fusion </td>
+		</tr>
+		
+		<tr>
+			<td> 3 </td>
+			<td> Ford </td>
+			<td> F-150 </td>
+		</tr>
+	
+	</table>
+	
 </body>
 
 </html>
