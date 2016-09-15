1983eb100703657f231f38d8b62f32d4a8fb888c
ckimberly97 <ckimberly97@southhills.edu>
2016-09-15 12:07:18 -0400

    Make the Table pretty
    Jarids fault

--- colton/basic-db-access/index.php
+++ colton/basic-db-access/index.php
@@ -4,41 +4,53 @@
 
 <head> 
 	<title>Basic Database Access</title>
+	<link> rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
 		<?= $result[0]["first_name"]; ?>
 		
+		<div class="table-title">
+			<h3>Data Table</h3>
+		</div>
 		
-		<table>
-		<?php foreach($result as $row): ?>
-			<tr>
-				<td><?= $row["id"]; ?></td>
-				<td><?= $row["first_name"]; ?></td>
-			</tr>
-		<?php endforeach; ?>
-		</table>
+	<table class="table-fill">
+		<thead>
+		<tbody class="table-hover">
+				<tr>
+					<th class="text-left">Id</th>
+					<th class="text-left">First Namw</th>
+				</tr>
+			</thead>
+			<?php foreach($result as $row): ?>
+				<tr>
+					<td> class=text-left<?= $row["id"]; ?></td>
+					<td> class=text-left<?= $row["first_name"]; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
 		
-		<p>The number of rows in the table: <?= $result->rowCount(); ?></p>
+	<p>The number of rows in the table: <?= $result->rowCount(); ?></p>
 		
-		<table>
-			<tr>
-				<td>1</td>
-				<td>Ford</td>
-				<td>Focus</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>Ford</td>
-				<td>Fusion</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>Ford</td>
-				<td>F150</td>
-			</tr>
+<table>
+	<tr>
+		<td>1</td>
+		<td>Ford</td>
+		<td>Focus</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Ford</td>
+		<td>Fusion</td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td>Ford</td>
+		<td>F150</td>
+	</tr>
 				
-		</table>		
+</table>		
 		
 </body>
 
