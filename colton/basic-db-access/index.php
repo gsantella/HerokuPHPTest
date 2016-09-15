397fe0da3ea734002a2e455607bb50ba038c0b11
ckimberly97 <ckimberly97@southhills.edu>
2016-09-15 11:26:34 -0400

    added emo table

--- colton/basic-db-access/index.php
+++ colton/basic-db-access/index.php
@@ -8,6 +8,26 @@
 
 <body>
 		<?= $result[0]["first_name"]; ?>
+		
+			<table>
+				<tr>
+					<td>1</td>
+					<td>Ford</td>
+					<td>Focus</td>
+				</tr>
+				<tr>
+					<td>2</td>
+					<td>Ford</td>
+					<td>Fusion</td>
+				</tr>
+				<tr>
+					<td>3</td>
+					<td>Ford</td>
+					<td>F150</td>
+				</tr>
+				
+			</table>		
+		
 </body>
 
 </html>
