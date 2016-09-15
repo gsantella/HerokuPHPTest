eafd8053e17f1dd619a955a4c2ba58610d8905fb
DarkFox216 <Darkfox216.@gmail.com>
2016-09-15 11:14:44 -0400

    test db_code

--- marcus/basic-db-access/index.php
+++ marcus/basic-db-access/index.php
@@ -1,19 +1,4 @@
-<?php
-//Create connection by logging in and selecting database
-$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
-$db = "ddf1ndt04b3tti";
-$user = "fpkkqmtaywxfwm";
-$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-
-$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
-$database = new PDO($dsn);
-//Run a SELECT SQL Statement to get results
-$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
-
-//Close Connection
-//Ignore for now
-
-?>
+<?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
 
@@ -21,7 +6,7 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 	<title>Basic Database Access</title>
 </head>
 <body>
-	<?php print_r($result); ?>
+	<?php $result[0]["first_name"]; ?>
 </body>
 
 </html>
