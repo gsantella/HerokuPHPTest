b27ef5a0768888ef501358b4c0a5eed8e633b784
dcarey50 <dcarey50@southhills.edu>
2016-09-15 11:12:53 -0400

    moved php to separate document

--- derek/basic-db-access/index.php
+++ derek/basic-db-access/index.php
@@ -1,25 +1,10 @@
-<?php
-//create connection by logging in and selecting database
-$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
-$db = "ddf1ndt04b3tti";
-$user = "fpkkqmtaywxfwm";
-$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-
-$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
-$database = new PDO($dsn);
-
-//Run a SELECT SQL Statement
-$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
-
-//close connection
-//ignore for now
-?>
+<?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>basic database access</title>
 </head>
 <body>
-	<?php print_r($result); ?> 
+	<?php print_r($result[0]["first_name"]); ?> 
 </body>
 </html>
