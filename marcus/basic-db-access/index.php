e88a92d0942b2db1870540de9f74b73d4aebc9a0
DarkFox216 <Darkfox216.@gmail.com>
2016-09-15 12:07:12 -0400

    Makes the table pretty

--- marcus/basic-db-access/index.php
+++ marcus/basic-db-access/index.php
@@ -4,18 +4,29 @@
 
 <head>
 	<title>Basic Database Access</title>
+	<link rel="stylesheet" href="css/main.css" />
 </head>
 
 <body>
 	<?=$result[0]["first_name"]; ?>
-	
-	<table>
-	<?php foreach($result as $row): ?>
-		<tr>
-			<td><?= $row["id"]; ?></td>
-			<td><?= $row["first_name"]; ?></td>
-		</tr>
-	<?php endforeach; ?>
+		<div class="table-title">
+	<h3>Data Table</h3>
+	</div>
+	<table class="table-fill">
+		<thead>
+			<tr>
+			<th class="text-left">Id</th>
+			<th class="text-left">First Name</th>
+			</tr>
+		</thead>
+	<tbody class="table-hover">
+		<?php foreach($result as $row): ?>
+			<tr>
+				<td class="text-left"><?= $row["id"]; ?></td>
+				<td class="text-left"><?= $row["first_name"]; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody class="table-hover">
 	</table>
 	
 	<p>The number of rows in the table: <?= $result->rowCount(); ?></p>
