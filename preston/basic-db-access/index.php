a9d831de0295d056846c8365ded79f5a88c5a0bc
diddymitchell23 <diddymitchell23@aol.com>
2016-09-15 11:14:26 -0400

    test db code!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- preston/basic-db-access/index.php
+++ preston/basic-db-access/index.php
@@ -1,33 +1,13 @@
-<?php
-//swassssss
-//Create connection by logging in and selecting database 
-$host= "ec2-23-21-234-201.compute-1.amazonaws.com";
-$db= "ddf1ndt04b3tti";
-$user= "fpkkqmtaywxfwm";
-$pass= "dbEE8plG9zaS_QCcVzZeD7maqU";
-
-$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
-$database = new PDO($dsn);
-
-//Run a SELECT SQL STATEMENT
-$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
-
-//Close connection  
-//Ignore for now 
-?>
-
+<?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
 
 <head>
 	<title>Basic Database Access</title>
-	
-
 </head>
 
 <body>
-	<?php print_r($result); ?>
-
+	<?= $result[0]["first_name"]; ?>
 </body>
 
 </html>
