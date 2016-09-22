2fd80e6871852377a8f07e062a78f75584a3de53
diddymitchell23 <diddymitchell23@aol.com>
2016-09-22 11:58:39 -0400

    ghjn

--- preston/kards/index.php
+++ preston/kards/index.php
@@ -74,7 +74,7 @@
    			<div class="col-twelve">
 
 	   			<h5>Hello, World.</h5>
-				<?php= Happy! date("Y/m/d") ?>
+				<?php= Happy! date("d") ?>
 	   			<h1>I'm Preston Mitchell.</h1>
 
 	   			<p class="intro-position">
