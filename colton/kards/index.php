bd2fcbb51559183a41c9382f0983f09070e60a9a
ckimberly97 <ckimberly97@southhills.edu>
2016-09-22 11:47:51 -0400

    PHP test updates again

--- colton/kards/index.php
+++ colton/kards/index.php
@@ -74,7 +74,7 @@
    			<div class="col-twelve">
 
 	   			<h5>Hello, World.</h5>
-				<?= style=color:Maroon Happy! date('l') ?>
+				<?= style="color:Maroon" Happy! date('l') ?>
 	   			<h1>I'm Colton Kimberly .</h1>
 
 	   			<p class="intro-position">
