e84beb23c8b5ea5f656824e4f797cb80eba3c3d2
diddymitchell23 <diddymitchell23@aol.com>
2016-09-22 11:52:41 -0400

    kjkn

--- preston/kards/index.php
+++ preston/kards/index.php
@@ -74,7 +74,7 @@
    			<div class="col-twelve">
 
 	   			<h5>Hello, World.</h5>
-				<?php style="color:blue" Happy date!('l') ?>
+				<?= Happy! date('l') ?>
 	   			<h1>I'm Preston Mitchell.</h1>
 
 	   			<p class="intro-position">
