bf756f9bc4ff182cb8b831ffc21fe369a4c0cdc7
dcarey50 <dcarey50@southhills.edu>
2016-09-27 11:47:14 -0400

    echo form submission

--- derek/kards/processContactForm.php
+++ derek/kards/processContactForm.php
@@ -1,3 +1,7 @@
 <?php
+echo "Name: " . $_REQUEST["ContactName"] . "<br />";
+echo "Email: " . $_REQUEST["ContactEmail"] . "<br />";
+echo "Subject: " . $_REQUEST["ContactSubject"] . "<br />";
+echo "Message: " . $_REQUEST["ContactMessage"] . "<br />";
 print_r($_REQUEST);
 ?>
