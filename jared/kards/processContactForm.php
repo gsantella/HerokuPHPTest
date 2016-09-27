6fb95a3407e6abe57c36e219121f4d134dfce38a
jsmith129540 <jsmith75@southhills.edu>
2016-09-27 11:48:44 -0400

    echo submit

--- jared/kards/processContactForm.php
+++ jared/kards/processContactForm.php
@@ -1,3 +1,8 @@
 <?php
-print_r($_REQUEST);
 
+echo "Name: " . $_REQUEST["contactName"] . "<br />";
+echo "Email: " . $_REQUEST["contactEmail"] . "<br />";
+echo "Subject: " . $_REQUEST["contactSubject"] . "<br />";
+echo "Message: " . $_REQUEST["contactMessage"] . "<br />";
+
+print_r($_REQUEST);
