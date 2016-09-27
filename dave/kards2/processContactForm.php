8bf9e71ca2a8deff96bfbda01812a7c7824ea4f7
dkelz16 <dkeller16x@yahoo.com>
2016-09-27 11:46:07 -0400

    php update

--- dave/kards2/processContactForm.php
+++ dave/kards2/processContactForm.php
@@ -1,7 +1,9 @@
 <?php
 
+echo "Name: " . $_REQUEST["contactName"] . "<br />";
+echo "Email: " . $_REQUEST["contactEmail"] . "<br />";
+echo "Subject: " . $_REQUEST["contactSubject"] . "<br />";
+echo "Message: " . $_REQUEST["contactMessage"] . "<br />";
+
+
 print_r($_REQUEST);
-
-
-
-
