a8c2050ead72afbf1e2b9e45721bc0ef8d4aa491
diddymitchell23 <diddymitchell23@aol.com>
2016-09-27 11:46:39 -0400

    updated contactform

--- preston/kards2/processContactForm.php
+++ preston/kards2/processContactForm.php
@@ -1,3 +1,8 @@
 <?php
 
+echo "Name: " . $_REQUEST["ContactName"] . "<br />";
+echo "Email: " . $_REQUEST["ContactEmail"] . "<br />";
+echo "Subject: " . $_REQUEST["ContactSubject"] . "<br />";
+echo "Message: " . $_REQUEST["ContactMesaage"] . "<br />";
+
 print_r($_REQUEST);
