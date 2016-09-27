e36c0c1f60072ce1ca6d30a1b65e49002641fe56
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-27 11:46:06 -0400

    added printing

--- allen/kards/processContactForm.php
+++ allen/kards/processContactForm.php
@@ -1,5 +1,10 @@
 <?php
 
+echo "Name: " . $_REQUEST["contactName"] ."<br/>";
+echo "Email: " . $_REQUEST["contactEmail"] ."<br/>";
+echo "Subject: " . $_REQUEST["contactSubject"] ."<br/>";
+echo "Message: " . $_REQUEST["contactMessage"] ."<br/>";
+
 
 print_r($_REQUEST);
 
