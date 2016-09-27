e31f91550b967d451de7bdefa3f9a4706815d862
DarkFox216 <Darkfox216.@gmail.com>
2016-09-27 12:09:12 -0400

    back to origin

--- marcus/Kards/processContactFrom.php
+++ marcus/Kards/processContactFrom.php
@@ -5,17 +5,6 @@ echo "Email: " . $_REQUEST["contactEmail"] . "<br />";
 echo "Subject: " . $_REQUEST["contactSubject"] . "<br />"; 
 echo "Message: " . $_REQUEST["contactMessage"] . "<br />"; 
 
-<div class="form-field">
-					   <input name="contactName" type="text" id="contactName" placeholder="Name" value="" minlength="2" required="">
-                  </div>
-                  <div class="form-field">
-	      			   <input name="contactEmail" type="email" id="contactEmail" placeholder="Email" value="" required="">
-	               </div>
-                  <div class="form-field">
-	     			   <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="">
-	               </div>                       
-                  <div class="form-field">
-					   <textarea name="contactMessage" id="contactMessage" placeholder="message" rows="10" cols="50" required=""></textarea>
-	               </div>                    
+                 
 
 print_r($_REQUEST)
