063af69413fb94bedf0077ac0421d12bb9bd76a9
DarkFox216 <Darkfox216.@gmail.com>
2016-09-27 12:12:49 -0400

    testing form

--- marcus/Kards/processContactFrom.php
+++ marcus/Kards/processContactFrom.php
@@ -5,6 +5,23 @@ echo "Email: " . $_REQUEST["contactEmail"] . "<br />";
 echo "Subject: " . $_REQUEST["contactSubject"] . "<br />"; 
 echo "Message: " . $_REQUEST["contactMessage"] . "<br />"; 
 
-                 
+    <div class="form-field">
+					   <input name="contactName" type="text" id="contactName" placeholder="Name" value="" minlength="2" required="">
+                  </div>
+				   <form>
+						<input type="radio" name="gender" value="male" > Male<br>
+						<input type="radio" name="gender" value="female"> Female<br>
+						<input type="radio" name="gender" value="other"> Other
+					</form>                
+                  <div class="form-field">
+	      			   <input name="contactEmail" type="email" id="contactEmail" placeholder="Email" value="" required="">
+	               </div>
+                  <div class="form-field">
+	     			   <input name="contactSubject" type="text" id="contactSubject" placeholder="Subject" value="">
+	               </div>                       
+                  <div class="form-field">
+					   <textarea name="contactMessage" id="contactMessage" placeholder="message" rows="10" cols="50" required=""></textarea>
+	               </div>                     
+
 
 print_r($_REQUEST)
