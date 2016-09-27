6a122b6c6e02e223296fca574fdb0ce40b82f972
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-27 11:33:50 -0400

    test disable javascript

--- allen/kards/index.php
+++ allen/kards/index.php
@@ -23,9 +23,9 @@
 
    <!-- script
    ================================================== -->   
-	<script src="js/modernizr.js"></script>
+	<!--<script src="js/modernizr.js"></script>
 	<script src="js/pace.min.js"></script>
-
+-->
    <!-- favicons
 	================================================== -->
 	<link rel="icon" type="image/png" href="favicon.png">
