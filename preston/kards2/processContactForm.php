eec11ff557f77bc1d4c13ad1fe2960b01e336df5
diddymitchell23 <diddymitchell23@aol.com>
2016-09-27 11:50:39 -0400

    weird i mispelled a word

--- preston/kards2/processContactForm.php
+++ preston/kards2/processContactForm.php
@@ -3,6 +3,6 @@
 echo "Name: " . $_REQUEST["contactName"] . "<br />";
 echo "Email: " . $_REQUEST["contactEmail"] . "<br />";
 echo "Subject: " . $_REQUEST["contactSubject"] . "<br />";
-echo "Message: " . $_REQUEST["contactMesaage"] . "<br />";
+echo "Message: " . $_REQUEST["contactMessage"] . "<br />";
 
 print_r($_REQUEST);
