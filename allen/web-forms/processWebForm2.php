d30efe4330ba10d9a17e1e2c0f6498a4924c901d
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-28 15:22:22 -0400

    added so it will write to file

--- allen/web-forms/processWebForm2.php
+++ allen/web-forms/processWebForm2.php
@@ -1,10 +1,10 @@
 <?php
 
 //open file for appending
-$file = fopen("testFile.txt", "a");
+$file = fopen("testfile.txt", "a");
 
 //Write POSTed variable "name" to file
-fwrite($_REQUEST["name"]. PHP_EOL);
+fwrite($file, $_REQUEST["name"]. PHP_EOL);
 
 //Be Nice! - Close the file
 fclose($file);
