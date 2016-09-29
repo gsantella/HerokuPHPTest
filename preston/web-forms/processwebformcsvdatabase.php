87b258c36f63a41fcaaf9812593b91e29de276d8
diddymitchell23 <diddymitchell23@aol.com>
2016-09-29 11:42:45 -0400

    fixed code

--- preston/web-forms/processwebformcsvdatabase.php
+++ preston/web-forms/processwebformcsvdatabase.php
@@ -3,7 +3,7 @@
 //Get data from webform
 $data = array($_REQUEST["first_name"],
 			 $_REQUEST["last_name"],
-			 $_REQUEST["favorite_color"]
+			 $_REQUEST["favorite_color"],
 			 $_REQUEST["password"]
 			 );
 
