78b859851785a7448ba7ad02280d351ef9774862
skywalkman2210 <ahazlett24@southhills.edu>
2016-09-29 11:23:32 -0400

    test csv

---
+++ allen/web-forms/processWebFormCsvDatabase.php
@@ -0,0 +1,18 @@
+<?php
+
+//Get data from webform
+$data = array(
+			$_REQUEST["first_name"],
+			$_REQUEST["last_name"],
+			$_REQUEST["favorite_color"],
+			$_REQUEST["password"]
+		);
+
+//open file for appending
+$database = fopen("database.csv", "a");
+
+//Write POSTed variable $data to file
+fputcsv($database, $data);
+
+//Be Nice! - Close the file
+fclose($database);
