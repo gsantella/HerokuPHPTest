1be0f85cae721177607fd151fabc99dd61a78e98
dcarey50 <dcarey50@southhills.edu>
2016-09-29 12:17:07 -0400

    i swear im not retarded

--- derek/web-forms/viewwebformcsvdatabase.php
+++ derek/web-forms/viewwebformcsvdatabase.php
@@ -7,7 +7,10 @@ while(!feof($database))
 {
 	$line = fgets($database);
 	$data = str_getcsv($line);
-	echo "First Name: ", $data[0];
+	echo "First Name: ", $data[0] . "<br />";
+	echo "Last Name: ", $data[1] . "<br />";
+	echo "Favorite Color: ", $data[2] . "<br />";
+	echo "Password: ", $data[3] . "<br />";
 }
 
 fclose($database);
