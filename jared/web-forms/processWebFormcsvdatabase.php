b7990ac0cb332519f54c79facdd9408807a0bde6
jsmith129540 <jsmith75@southhills.edu>
2016-09-29 11:31:48 -0400

    fixxed processwebformcsvdatabase

--- jared/web-forms/processWebFormcsvdatabase.php
+++ jared/web-forms/processWebFormcsvdatabase.php
@@ -1,3 +1,16 @@
 <?php
+//Get data from webform
+$data = array( $_REQUEST["first_name"], 
+			   $_REQUEST["last_name"], 
+			   $_REQUEST["favorite_color"],
+			   $_REQUEST["password"]
+			 );
 
-print_r($_REQUEST);
+//Open file database.csv for appending
+$database = fopen("database.csv", "a");
+
+//Write POSTed $data to file
+fputcsv($database, $data);
+
+//Close the file
+fclose($database);
