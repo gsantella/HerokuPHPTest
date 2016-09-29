71d3eb9abce1fe89ca6458e2e96b4227b79265dd
dcarey50 <dcarey50@southhills.edu>
2016-09-29 11:41:46 -0400

    does this work yet?

--- derek/web-forms/processwebformcsvdatabase.php
+++ derek/web-forms/processwebformcsvdatabase.php
@@ -1,17 +1,9 @@
 <?php
-
-//Get data from webform
 $data = array( $_REQUEST["first_name"], 
 			   $_REQUEST["last_name"], 
 			   $_REQUEST["favorite_color"],
 			   $_REQUEST["password"]
 			 );
-
-//Open file database.csv for appending
 $database = fopen("database.csv", "a");
-
-//Write POSTed $data to file
 fputcsv($database, $data);
-
-//Close the file
-fclose($database);
+print_r($_REQUEST);
