44e76d367f60e511afeb584e368e86f70890da5f
ghill714 <wosemwegie88@southhills.edu>
2016-09-29 12:05:44 -0400

    save changes2

--- will/web-forms/viewwebformcsvdatabase.php
+++ will/web-forms/viewwebformcsvdatabase.php
@@ -1,1 +1,21 @@
 <?php
+
+//Get data from webform
+$data = array( $_REQUEST["first_name"],
+			   $_REQUEST["last_name"],
+			   $_REQUEST["favorite_color"],
+			   $_REQUEST["password"]
+			  );
+			  
+//Open file database
+$database = fopen("database.csv", "r");
+
+//Read each line and print to screen
+while(!feof($database))
+{
+	$line= fgets($database);
+	echo $line;
+}
+
+//close the database
+fclose($database);
