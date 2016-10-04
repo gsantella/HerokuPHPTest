3843b77d2cffe2a11e1109d854298afb1f03e49f
ckimberly97 <ckimberly97@southhills.edu>
2016-10-04 11:45:57 -0400

    CRUD update

--- colton/basic-db-access/addUser.php
+++ colton/basic-db-access/addUser.php
@@ -2,7 +2,7 @@
 
 	$firstName = $_POST['firstName'];
 	
-	include("../db_functions.php");
+	include("../inc/db_functions.php");
 	
 	insert("INSERT INTO students (first_name) VALUES ('$firstName') ");
 	
