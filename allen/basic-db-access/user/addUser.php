0edff41e7dcd3873434d1fbe97ee30ba3df063f6
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-04 11:48:23 -0400

    some error checking

--- allen/basic-db-access/user/addUser.php
+++ allen/basic-db-access/user/addUser.php
@@ -1,9 +1,16 @@
 <?php
 
-	$firstName = $_POST['firstName'];
-	
-	include("../inc/db_fuctions.php");
-	
-	insert("INSERT INTO students (first_name) VALUES ('$firstName') ");
-	
-	Header("Location: ../");
+	if(isset($_POST['firstName']))
+	{
+		$firstName = $_POST['firstName'];
+		
+		include("../inc/db_fuctions.php");
+		
+		insert("INSERT INTO students (first_name) VALUES ('$firstName') ");
+		
+		Header("Location: ../");
+	}
+	else
+	{
+		echo "The firstName was not passed!";
+	}
