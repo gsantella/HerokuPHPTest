a38183671c166d650e82cf40c9ed9195232744ea
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-04 11:38:54 -0400

    fixed a few things with Capitalization

--- allen/basic-db-access/index.php
+++ allen/basic-db-access/index.php
@@ -27,7 +27,7 @@
 		</table>
 		
 		<p id="addPerson">
-			<form action="user/adduser.php">
+			<form action="user/addUser.php" method="post">
 				<input type="text" name="firstName" />
 				<input type="submit" value="Add Person" />
 			</form>
