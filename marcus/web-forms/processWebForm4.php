2f50ca74b8d19421f3467e6f5a3f4e487e5fa208
DarkFox216 <Darkfox216.@gmail.com>
2016-10-04 12:11:03 -0400

    Created webform4

---
+++ marcus/web-forms/processWebForm4.php
@@ -0,0 +1,29 @@
+<DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Basic Webform With CSV Database</title>
+</head>
+
+<body>
+<<<<<<< HEAD
+<<<<<<< HEAD:dave/web-forms/webformcsvdatabase.html
+<a href="viewwebformcsvdatabase.php">View Data</a>
+	<form method="post" action=viewwebformcsvdatabase.php>
+=======
+=======
+	<a href="viewWebFormcsvdatabase.php">View Data</a>
+>>>>>>> origin/master
+	<form method="post" action="processWebFormcsvdatabase.php">
+>>>>>>> origin/master:marcus/web-forms/webformcsvdatabase.html
+		Enter Your First Name <input type="text" name="first_name" /><br />
+		Enter Your Last Name <input type="text" name="last_name" /><br />
+		Enter Your Address <input type="text" name="address" /><br />
+		Enter Your Favorite Color <input type="text" name="favorite_color" /><br />
+		Enter Your Password <input type="password" name="password" /><br />
+		<input type="submit"  value="Submit" /><br />
+	</form>
+</body>
+	
+</html>
