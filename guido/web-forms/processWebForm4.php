2863aec789bedc770d5f9cda8c4666d265f8ef4f
gsantella <gsantella@southhills.edu>
2016-10-04 11:38:55 -0400

    Try at database insert

---
+++ guido/web-forms/processWebForm4.php
@@ -0,0 +1,25 @@
+<?php
+
+//Create Connection : Login + Select Database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+$database = new PDO($dsn);
+
+//Run a INSERT SQL Statement
+$name = $_REQUEST["name"];
+$stmt = $database->prepare("INSERT INTO table(name) VALUES(?)");
+$stmt->execute(array($name));
+
+//Display affected rows
+$affected_rows = $stmt->rowCount();
+echo "$affected_rows have been affected!";
+
+//Link to R in CRUD
+echo "<a href='../basic-db-access'>View Data</a>";
+
+//Close Connection
+//Ignore For Now . . 
