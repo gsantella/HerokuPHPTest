85441694e2d4d972323283c68688aec07b95dade
ghill714 <wosemwegie88@southhills.edu>
2016-10-05 15:22:43 -0400

    uplaod trial forms

--- will/basic-db-access/inc/processformA.php
+++ will/basic-db-access/inc/processformA.php
@@ -1,7 +1,15 @@
 <?php
-$data = array( $_REQUEST["id"],
-			   $_REQUEST["first_name"]
-			 );
-<img src="<?= $targetFile ?>" />
-?>
+//$data = array( $_REQUEST["id"],
+//			   $_REQUEST["first_name"]
+//			 );
+// <img src="<?= $targetFile  ?" />
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
 
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+$database = new PDO($dsn);
+
+$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+?>
