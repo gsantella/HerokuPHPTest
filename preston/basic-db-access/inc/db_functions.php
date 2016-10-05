5df1bec26debf37adc6936a7d865c4aa52df41a8
diddymitchell23 <diddymitchell23@aol.com>
2016-10-05 14:39:23 -0400

    8k777yukuy

--- preston/basic-db-access/inc/db_functions.php
+++ preston/basic-db-access/inc/db_functions.php
@@ -1,4 +1,23 @@
 <?php
+//Webform
+//Get data from webform
+$data = array($_REQUEST["first_name"],
+			 $_REQUEST["last_name"],
+			 $_REQUEST["favorite_color"],
+			 $_REQUEST["password"]
+			 );
+
+//Open file database.csv for appending 
+$database = fopen("database.csv", "a");
+
+//Write POSTed $data to file
+fputcsv($database, $data);
+
+//Close the file
+fclose($database);
+
+
+
 //swassssss
 //Create connection by logging in and selecting database 
 $host= "ec2-23-21-234-201.compute-1.amazonaws.com";
@@ -11,6 +30,8 @@ $database = new PDO($dsn);
 
 //Run a SELECT SQL STATEMENT
 $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 //Close connection  
 //Ignore for now 
--- preston/basic-db-access/index.php
+++ preston/basic-db-access/index.php
@@ -1,35 +1,5 @@
 <?php include("inc/db_functions.php"); 
-//Get data from webform
-$data = array($_REQUEST["first_name"],
-			 $_REQUEST["last_name"],
-			 $_REQUEST["favorite_color"],
-			 $_REQUEST["password"]
-			 );
 
-//Open file database.csv for appending 
-$database = fopen("database.csv", "a");
-
-//Write POSTed $data to file
-fputcsv($database, $data);
-
-//Close the file
-fclose($database);
-//Open file database.csv for appending 
-$database = fopen("database.csv", "r");
-
-//Read each line and print to screen 
-while(!feof($database))
-{
-	$line = fgets($database);
-	$data = str_getcsv($line);
- 	echo "First Name: " . $data[0] . "<br />";
-	echo "Last Name: " . $data[1] . "<br />";
-	echo "Favorite Color: " . $data[2] . "<br />";
-	echo "Password: " . $data[3] . "<br />";
-}
-	
-//Close the file
-fclose($database);
 ?>
 
 <!DOCTYPE html>
