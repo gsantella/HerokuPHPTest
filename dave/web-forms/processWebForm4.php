0bfcf864322b323cc3d24b0274e7ea34702b731a
dkelz16 <dkeller16x@yahoo.com>
2016-10-05 15:12:27 -0400

    1

--- dave/web-forms/processWebForm4.php
+++ dave/web-forms/processWebForm4.php
@@ -1,8 +1,19 @@
+
 <?php
+//Create Connection : Login + Select Database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
 
-	$data = [$_REQUEST["name"];
-	
-	$pdo = $database->prepare("INSERT INTO students(firstName) VALUES($data)");
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+$database = new PDO($dsn);
 
-?>
 
+
+
+$pdo->prepare("INSERT INTO users VALUES (NULL,?,?,?,?)")->execute();
+
+
+//Close Connection
+//Ignore For Now . . 
