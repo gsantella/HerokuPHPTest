70c1f0f32d07375a465ff491a1f0f7b80e8e0635
jsmith129540 <jsmith75@southhills.edu>
2016-10-05 15:34:32 -0400

    added insert

---
+++ jared/web-forms/groupInsertForm.php
@@ -0,0 +1,17 @@
+<?php
+//Create connection by loggin in and selecting database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+	    //POST Gress
+$database = new PDO($dsn);
+
+$data = array($_REQUEST["name"));
+
+$database->prepare("INSERT INTO students(first_name) values(?)")->execute($data);
+
+//Close connection
+//IGNORE FOR NOW
