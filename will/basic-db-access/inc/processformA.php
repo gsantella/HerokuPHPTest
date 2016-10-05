33668516bdd6e50a9ba8808451daf61ef17a7880
ghill714 <wosemwegie88@southhills.edu>
2016-10-05 15:50:58 -0400

    update webform

--- will/basic-db-access/inc/processformA.php
+++ will/basic-db-access/inc/processformA.php
@@ -1,15 +1,16 @@
 <?php
-//$data = array( $_REQUEST["id"],
-//			   $_REQUEST["first_name"]
-//			 );
-// <img src="<?= $targetFile  ?" />
+
 $host = "ec2-23-21-234-201.compute-1.amazonaws.com";
 $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
 $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-
 $dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 
 $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+
+$data = array( $_REQUEST["first_name"]);
+
+$stmt = $database->prepare("INSERT INTO students (first_name) VALUES(?)");
+$stmt->EXECUTE($data);
 ?>
