2a6b02aa1331577f08b5832ab0a016a17c74ac64
diddymitchell23 <diddymitchell23@aol.com>
2016-10-05 14:46:33 -0400

    update

--- preston/basic-db-access/inc/db_functions.php
+++ preston/basic-db-access/inc/db_functions.php
@@ -1,22 +1,4 @@
 <?php
-//Webform
-//Get data from webform
-$data = array($_REQUEST["first_name"],
-			 $_REQUEST["last_name"],
-			 $_REQUEST["favorite_color"],
-			 $_REQUEST["password"]
-			 );
-
-//Open file database.csv for appending 
-$database = fopen("database.csv", "a");
-
-//Write POSTed $data to file
-fputcsv($database, $data);
-
-//Close the file
-fclose($database);
-
-
 
 //swassssss
 //Create connection by logging in and selecting database 
@@ -36,3 +18,4 @@ $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC
 //Close connection  
 //Ignore for now 
 ?>
+
--- preston/web-forms/viewwebformcsvdatabase.php
+++ preston/web-forms/viewwebformcsvdatabase.php
@@ -9,9 +9,7 @@ while(!feof($database))
 	$line = fgets($database);
 	$data = str_getcsv($line);
  	echo "First Name: " . $data[0] . "<br />";
-	echo "Last Name: " . $data[1] . "<br />";
-	echo "Favorite Color: " . $data[2] . "<br />";
-	echo "Password: " . $data[3] . "<br />";
+
 }
 	
 //Close the file
