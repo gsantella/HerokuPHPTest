6a82cdcf0d5e3115bf65b4bccddb103c01a7b519
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-06 15:31:49 -0400

    figure code that is messing up?

--- allen/basic-db-access/user/modifyUser.php
+++ allen/basic-db-access/user/modifyUser.php
@@ -25,7 +25,7 @@
 	}
 	else
 	{
-		if(isset($_REQUEST['modId']))
+		/*if(isset($_REQUEST['modId']))
 		{
 			$id = $_REQUEST['modId'];
 			
@@ -39,7 +39,7 @@
 					</tr> \
 				</table>";
 			
-		}
+		}*/
 	}
 ?>
 
