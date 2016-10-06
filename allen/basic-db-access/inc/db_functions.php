6f8207816011aded77c5b384f0041dda169b1733
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-06 13:07:38 -0400

    removed single quotes

--- allen/basic-db-access/inc/db_functions.php
+++ allen/basic-db-access/inc/db_functions.php
@@ -21,8 +21,8 @@
 		$stmt->execute();
 	}
 	
-	function modify($id, $name){
-		$stmt= $database->prepare("UPDATE students SET first_name = ':name' WHERE id=:id ");
+	function modify($id, $name) {
+		$stmt= $database->prepare("UPDATE students SET first_name=:name WHERE id=:id ");
 		$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 		$stmt->execute();
