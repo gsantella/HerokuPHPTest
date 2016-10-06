5fb1b75c0603fc8b8878aed20eb3c356552568ba
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-06 12:47:59 -0400

    test with PARAM_INT

--- allen/basic-db-access/user/deleteUser.php
+++ allen/basic-db-access/user/deleteUser.php
@@ -6,7 +6,7 @@
 		$id = $_POST['delId'];
 		
 		$stmt= $database->prepare("DELETE FROM 'students' WHERE 'id' = :id;");
-		$stmt->bindValue(':id', $id);
+		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 		$stmt->execute();
 		
 		echo $stmt->rowCount() . " Rows Affected.";
