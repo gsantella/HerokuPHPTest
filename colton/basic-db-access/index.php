ac5326ef0ab53851ff84c44a01f15052c62182dc
ckimberly97 <ckimberly97@southhills.edu>
2016-10-06 12:45:06 -0400

    fixed create statements

--- colton/basic-db-access/index.php
+++ colton/basic-db-access/index.php
@@ -1,42 +1,41 @@
-<?php include("inc/db_functions.php"); ?> 
+<?php include("inc/db_functions.php"); 
+
+?>
+
 <!DOCTYPE html>
 <html>
 
-	<head> 
-		<title>Basic Database Access</title>
-		<link rel="stylesheet" href="css/main.css" />
-	</head>
+<head>
+	<title>Basic Database Access</title>
+	<link rel="stylesheet" href="css/main.css" />
+</head>
 
-		<body>
-				<?= $result[0]["first_name"]; ?>
-				
-				<div class="table-title">
-					<h3>Data Table</h3>
-				</div>
-				
-			<table class="table-fill">
-				<thead>
-				<tbody class="table-hover">
-						<tr>
-							<th class="text-left">Id</th>
-							<th class="text-left">First Name</th>
-						</tr>
-					</thead>
-					<?php foreach($result as $row): ?>
-						<tr>
-							<td class="text-left"><?= $row["id"]; ?></td>
-							<td class="text-left"><?= $row["first_name"]; ?></td>
-						</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
-			
-		<p id="addPerson">
-		<form action="addUser.php" method="post">
-			<input type="text" name="firstName" />
-			<input type="submit" value="Add Person" />
-		</form>
-		
-	</body>
+<body>
+	<?= $result[0]["first_name"]; ?>
+	
+	
+</body>
+
+	<div class="table-title">
+	<h3>Data Table</h3>
+	</div>
+
+	<table class="table-fill">
+	<thead>
+	<tr>
+		<th class="text-left">ID</th>
+		<th class="text-left">First Name</th>
+	</tr>
+	</thead>
+	<tbody class="table-hover">
+	<?php foreach($result as $row): ?>
+		<tr>
+			<td class="text-left"><?= $row["id"]; ?></td>
+			<td class="text-left"><?= $row["first_name"]; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</table>
+
+
 
 </html>
