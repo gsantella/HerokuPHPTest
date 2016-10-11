95b2b356d2f60c0540b0980a2dcd5d024b2f7d95
gsantella <gsantella@southhills.edu>
2016-10-11 12:10:16 -0400

    attempt getAllData

--- guido/ModernDatabaseCRUD/inc/database.class.php
+++ guido/ModernDatabaseCRUD/inc/database.class.php
@@ -6,6 +6,17 @@ class Database {
 	private $dbUsername = "fpkkqmtaywxfwm";
 	private $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
 	
+	public function __construct()
+	{
+		$dsn = "pgsql:dbname=$this->dbName;host=$this->dbHost;user=$this->dbUserName;password=$this->dbPassword";
+		$this->db = new PDO($dsn);
+	}
+	
+	public function getAllData()
+	{
+		return $this->db->query("SELECT * FROM students;")->fetchALL(PDO::FETCH_ASSOC);
+	}
+	
 	public function getDatabaseUsername()
 	{
 		return $this->dbUsername;
