b9fbf90b4bcd23c96fe18cc5bb1c9f9f75073ce7
gsantella <gsantella@southhills.edu>
2016-10-11 12:14:39 -0400

    misspelling

--- guido/ModernDatabaseCRUD/index.php
+++ guido/ModernDatabaseCRUD/index.php
@@ -14,3 +14,9 @@ $db = new Database();
 
 // Call the getAllData function and print what returns
 print_r($db->getAllData());
+
+
+//$db->create();
+//$db->retrieve();
+//$db->update();
+//$db->delete();
