30c09ba358e935372100a4fd7c03ff30f3ed2a05
diddymitchell23 <diddymitchell23@aol.com>
2016-10-11 11:40:34 -0400

    im stupid

--- preston/ModernDatabaseCRUD/inc/database.class.php
+++ preston/ModernDatabaseCRUD/inc/database.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class Database {
-	public &dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
+	public $dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
 	public $dbName = "ddf1ndt04b3tti";
 	public $dbUsername = "fpkkqmtaywxfwm";
 	public $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
