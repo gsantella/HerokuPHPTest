c71c6f2631b59b7fc534aee1b5f33c317dcbf360
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-11 12:02:37 -0400

    added contructor

--- allen/moderndatabasecrud/inc/database.class.php
+++ allen/moderndatabasecrud/inc/database.class.php
@@ -7,21 +7,24 @@
 		private $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
 		private $dbName = "ddf1ndt04b3tti";
 		
-		//public $dsn = "pgsql:dbname=$dbName;host=$dbHost;user=$dbUsername;password=$dbPassword";
+		public function __construct() {
+			$dsn = "pgsql:dbname=$this->dbName;host=$this->dbHost;user=$this->dbUsername;password=$this->dbPassword";
+			$this->db = new PDO($dsn);
+		}
 		
 		public function getDatabaseUsername() {
 			return $this->dbUsername;
 		}
 		
-		function addRecord($firstName, $lastName) {
+		public function addRecord($firstName, $lastName) {
 			
 		}
 		
-		function deleteRecord($id) {
+		public function deleteRecord($id) {
 			
 		}
 		
-		function save() {
+		public function save() {
 			
 		}
 		
