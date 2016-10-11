b8b1fdf4a5f79ccb9c42c364530860500df5b752
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-11 11:36:41 -0400

    changed private to public

--- allen/moderndatabasecrud/inc/database.class.php
+++ allen/moderndatabasecrud/inc/database.class.php
@@ -2,12 +2,12 @@
 
 	class Database {
 		
-		private $dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
-		private $dbUsername = "fpkkqmtaywxfwm";
-		private $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
-		private $dbName = "ddf1ndt04b3tti";
+		public $dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
+		public $dbUsername = "fpkkqmtaywxfwm";
+		public $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
+		public $dbName = "ddf1ndt04b3tti";
 		
-		private $dsn = "pgsql:dbname=$dbName;host=$dbHost;user=$dbUsername;password=$dbPassword";
+		public $dsn = "pgsql:dbname=$dbName;host=$dbHost;user=$dbUsername;password=$dbPassword";
 		
 		/*
 		function addRecord($firstName, $lastName) {
