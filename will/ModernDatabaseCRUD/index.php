77e10c63854d280372203f3c7c941bcfa58f8615
ghill714 <wosemwegie88@southhills.edu>
2016-10-11 11:36:59 -0400

    basic class experiment

---
+++ will/ModernDatabaseCRUD/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//include the database class
+include("inc/database.class.php");
+
+//make a database object called $db
+$db = new Database();
+
+//Access the $dbHost variable in the Database object
+echo $db->dbHost;
