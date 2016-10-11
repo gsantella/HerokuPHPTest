b5c2a2e12cb1b9f98aef8ae2f25f8778d4be8aba
dkelz16 <dkeller16x@yahoo.com>
2016-10-11 11:36:48 -0400

    php database

---
+++ dave/ModernDatabaseCRUD/index.php
@@ -0,0 +1,9 @@
+<?php
+//INCLUDES THE DATABASE CLASS
+include("inc/database.class.php");
+
+//MAKE A DATABASE OBJECT CALLED $db
+$db = new Database();
+
+//ACCESS THE $dbHost VARIABLE IN THE OBJECT
+echo $db->dbHost;
