ad3c58eac743a9782a4df2685cf78d418f61509d
ghill714 <wosemwegie88@southhills.edu>
2016-10-11 11:40:24 -0400

    private experiment

--- will/ModernDatabaseCRUD/inc/database.class.php
+++ will/ModernDatabaseCRUD/inc/database.class.php
@@ -3,8 +3,8 @@
 //$db = new Database();
 
 class Database {
-	public $dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
-	public $dbName = "ddf1ndt04b3tti";
-	public $dbUsername = "fpkkqmtaywxfwm";
-	public $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
+	private $dbHost = "ec2-23-21-234-201.compute-1.amazonaws.com";
+	private $dbName = "ddf1ndt04b3tti";
+	private $dbUsername = "fpkkqmtaywxfwm";
+	private $dbPassword = "dbEE8plG9zaS_QCcVzZeD7maqU";
 }
