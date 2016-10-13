efd59e5e7ef70fa98fb2d7449f1e757ed6dc618e
DarkFox216 <Darkfox216.@gmail.com>
2016-10-13 11:21:34 -0400

    example of OOP

--- marcus/ModernDatabaseCRUD/index.php
+++ marcus/ModernDatabaseCRUD/index.php
@@ -6,12 +6,10 @@ include("inc/database.class.php");
 //Make a database object called $db
 $db = new database();
 
-
-// Access the $dbHost variable in the Database object
-//echo $db->dbHost;
-
-//Call the getDatabaseUsername function from the Database object
-//echo $db->getDatabaseUsername();
-
 // Call the getAllData function and print what returns
 print_r($db->getAllData());
+
+//$db->create();
+//$db->retrieve();
+//$db->update();
+//$db->delete();
