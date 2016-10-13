ba01074956a653d95210434ea184005194fd4ef7
diddymitchell23 <diddymitchell23@aol.com>
2016-10-13 11:43:16 -0400

    CRUD

--- preston/ModernDatabaseCRUD/index.php
+++ preston/ModernDatabaseCRUD/index.php
@@ -6,11 +6,10 @@ include("inc/database.class.php");
 // Make a Dtabase object called $dba_close
 $db = new Database();
 
-// Access the $dbHost variable in the Database object 
-//echo $db->dbHost;
-
-// Call the getDatabaseUsername function from the Database object 
-// echo $db->getDatabaseUsername();
-
 // Call the getAllData function and print what returns 
 print_r($db->getAllData());
+
+// Test that the insert function worked 
+$db->insert("123456 - Preston");
+$db->insert("696969 - Diddy");
+$db->insert("232323 - Mitchell");
