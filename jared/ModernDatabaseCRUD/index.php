74bb5d670b05c1be7614fe0e1af6cc4d2fe67bbf
jsmith129540 <jsmith75@southhills.edu>
2016-10-13 12:17:24 -0400

    testing progress bar

--- jared/ModernDatabaseCRUD/index.php
+++ jared/ModernDatabaseCRUD/index.php
@@ -16,6 +16,6 @@ $db = new Database();
 print_r($db->getAllData());
 
 //test that the insert function works
-$db->insert("8128 - Jared");
-$db->insert("8178 - Jared");
-$db->insert("8188 - Jared");
+$db->insert("8128-Jared");
+$db->insert("8178-Jared");
+$db->insert("8188-Jared");
