f31909bcc425ed36ffec5c240539ffbe0e3bc75e
gsantella <gsantella@southhills.edu>
2016-10-13 11:20:45 -0400

    Example of OOP

--- guido/ModernDatabaseCRUD/index.php
+++ guido/ModernDatabaseCRUD/index.php
@@ -6,14 +6,9 @@ include("inc/database.class.php");
 // Make a Database object called $db
 $db = new Database();
 
-// Access the $dbHost variable in the Database object
-//echo $db->dbHost;
-
-// Call the getDatabaseUsername function from the Database object
-//echo $db->getDatabaseUsername();
-
 // Call the getAllData function and print what returns
 print_r($db->getAllData());
+
 
 
 //$db->create();
