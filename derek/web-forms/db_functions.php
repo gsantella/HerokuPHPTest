5fae763bd720f6e8c0e829931b7fc5183a1c4f80
dcarey50 <dcarey50@southhills.edu>
2016-10-17 15:35:28 -0400

    bugtesting

--- derek/web-forms/db_functions.php
+++ derek/web-forms/db_functions.php
@@ -4,14 +4,13 @@ $host = "ec2-23-21-234-201.compute-1.amazonaws.com";
 $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
 $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
-$_REQUEST["name"];
 $dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 $name = $_REQUEST["name"];
 //Run a SELECT SQL Statement
 $stmt = $db->prepare("INSERT into students(first_name) VALUES (?)");
 $stmt->execute(array($name));
-echo $name;
+echo $_REQUEST;
 
 //close connection
 //ignore for now
