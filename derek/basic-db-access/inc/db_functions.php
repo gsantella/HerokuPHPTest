717290fe35915504ab47605b9c5f46bc8632a26b
dcarey50 <dcarey50@southhills.edu>
2016-10-17 15:52:54 -0400

    working database viewer

--- derek/basic-db-access/inc/db_functions.php
+++ derek/basic-db-access/inc/db_functions.php
@@ -1,5 +1,6 @@
 <?php
-//create connection by logging in and selecting database
+
+//Create Connection : Login + Select Database
 $host = "ec2-23-21-234-201.compute-1.amazonaws.com";
 $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
@@ -8,9 +9,13 @@ $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
 $dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 
-//Run a SELECT SQL Statement
+//Run a SELECT SQL Statement To Get Results
 $result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
 
-//close connection
-//ignore for now
-?>
+//To Update SQL Statement
+//$stmt = $db->prepare("UPDATE students SET name=? WHERE id=?")
+//$stmt -> execute(array($name, $id));
+//$affected_rows = $stmt -> rowCount();
+
+//Close Connection
+//Ignore For Now . . 
--- derek/basic-db-access/index.php
+++ derek/basic-db-access/index.php
@@ -1,37 +1,54 @@
 <?php include("inc/db_functions.php"); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>basic database access</title>
+	<title>Basic Database Access</title>
+	<link rel="stylesheet" href="css/main.css" />
 </head>
+
 <body>
-	<?=($result[0]["first_name"]); ?>
+	<?= $result[0]["first_name"]; ?>
+	
 	<div class="table-title">
 		<h3>Data Table</h3>
 	</div>
 	<table class="table-fill">
-	<thead>
-	
-	<tr>
-		<th class="text-left">ID</th>
-		<th class="text-left">First Name</th>
-	</tr>
-	</thead>
-	<tbody class="table-hover">
-		<?php foreach($result as $row): ?>
+		<thead>
 			<tr>
-				<td><?= $row["id"]; ?></td>
-				<td><?= $row["first_name"]; ?></td>
+				<th class="text-left">Id</th>
+				<th class="text-left">First Name</th>
 			</tr>
-		<?php endforeach ; ?>
-	</tbody>
+		</thead>
+		<tbody class="table-hover">
+			<?php foreach($result as $row): ?>
+				<tr>
+					<td class="text-left"><?= $row["id"]; ?></td>
+					<td class="text-left"><?= $row["first_name"]; ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
 	</table>
-		
-	<p>the number of rows in the table: <?= $result->rowCount(); ?></p>
+	
+	<p>The number of rows in the table: <?= $result->rowCount(); ?></p>
+	
 	<table>
-		<tr><td>1</td><td>Ford</td><td>Focus</td></tr>
-		<tr><td>2</td><td>Ford</td><td>Fusion</td></tr>
-		<tr><td>3</td><td>Ford</td><td>F150</td></tr>
+		<tr>
+			<td>1</td>
+			<td>Ford</td>
+			<td>Focus</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>Ford</td>
+			<td>Fusion</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td>Ford</td>
+			<td>F150</td>
+		</tr>
 	</table>
 </body>
+
 </html>
