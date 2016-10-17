5f0896b254682ab2ed8505f5e0aee02c4dd17536
dcarey50 <dcarey50@southhills.edu>
2016-10-17 15:45:20 -0400

    syntax fixes

--- derek/web-forms/db_functions.php
+++ derek/web-forms/db_functions.php
@@ -1,15 +1,17 @@
 <?php
+
 //create connection by logging in and selecting database
 $host = "ec2-23-21-234-201.compute-1.amazonaws.com";
 $db = "ddf1ndt04b3tti";
 $user = "fpkkqmtaywxfwm";
 $pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
 $dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
 $database = new PDO($dsn);
 
+//Run a INSERT SQL Statement
 $name = $_REQUEST["name"];
-//Run a SELECT SQL Statement
-$stmt = $database->prepare("INSERT into students(first_name) VALUES (?);");
+$stmt = $database->prepare("INSERT INTO students(first_name) VALUES(?);");
 $stmt->execute(array($name));
 echo $_REQUEST["name"];
 
