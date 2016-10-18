ada1f864a0fc0317a1c024f5effb89ac58ce5b78
dkelz16 <dkeller16x@yahoo.com>
2016-10-18 11:41:45 -0400

    test alert

--- dave/ModernDatabaseCRUD/webform.php
+++ dave/ModernDatabaseCRUD/webform.php
@@ -8,12 +8,14 @@
 </head>
 
 <body>
-	<button id="btnLoadDataWithAJAX">Click Me For Data</button>
+	<button id="btnLoadDataWithAJAX" onclick="myFunction()">Click Me For Data</button>
+	
 	<div id="data">
 		Data Will Be Updated Here Without A Page Reload! - YAY!
 	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 	
 </html>
