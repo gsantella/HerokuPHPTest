0d575f95229105053bdb27c6311ed6ba2ed70b29
gsantella <gsantella@southhills.edu>
2016-10-18 11:32:49 -0400

    add button text

---
+++ guido/ModernDatabaseCRUD/webform.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Modern Webform</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+
+<button id="btnLoadDataWithAJAX">Click me for data!</button>
+
+<div id="data">
+	Data will be updated here without a page reload!  
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
