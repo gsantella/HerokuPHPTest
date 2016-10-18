90b88daaafa5a0fd2d91784c8e65e5ae8f996a51
DarkFox216 <Darkfox216.@gmail.com>
2016-10-18 12:08:31 -0400

    changed to title

--- marcus/ModernDatabaseCRUD/js/main.js
+++ marcus/ModernDatabaseCRUD/js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now, hold on, and don't rush me!");
-		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data)) {
-				alert(data.toSource());
-				$("#data").html(data.toSource());
+		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data) {
+				alert(data.title());
+				$("#data").html(data.title());
 		)};
 	
 	});
