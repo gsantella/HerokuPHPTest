ffbafd1c9f79715f0c6f88c5644529597585045d
DarkFox216 <Darkfox216.@gmail.com>
2016-10-18 11:56:06 -0400

    removed ajax

--- marcus/ModernDatabaseCRUD/js/main.js
+++ marcus/ModernDatabaseCRUD/js/main.js
@@ -2,12 +2,8 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() 
 		alert("I'm going to get your data now, hold on, and don't rush me!")
-		$.ajax({
-			type:'post',
-			url:'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
-			success: function(json) {
-				$("#data").html(json);
-			}
+		$.getJSON({'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data) {
+				$("#data").html(data);
 		)};
 	
 	});
