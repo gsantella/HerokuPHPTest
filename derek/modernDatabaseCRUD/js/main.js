be9fc4e26e022ff68a466a11b5c6cf2c73fd8aec
dcarey50 <dcarey50@southhills.edu>
2016-10-18 11:53:31 -0400

    changes

---
+++ derek/modernDatabaseCRUD/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	$("#btnLoadDataWithAJAX").click(function() {
+		alert("I'm going to get your data now!");
+		$.ajax({
+			type: 'POST',
+			url: 'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
+			success: function(json) {
+				$("#data").html(json);
+			}
+		});
+	});
+		
+});
