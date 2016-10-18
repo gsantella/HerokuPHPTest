34e559b6092208a310e40ac478b4c1ad517547bf
DarkFox216 <Darkfox216.@gmail.com>
2016-10-18 12:13:32 -0400

    removed )

--- marcus/ModernDatabaseCRUD/js/main.js
+++ marcus/ModernDatabaseCRUD/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now, hold on, and don't rush me!");
-		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data)) {
+		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data) {
 				alert(data.title);
 				$("#data").html(data.title);
 		)};
