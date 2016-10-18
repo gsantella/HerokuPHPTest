99265744cbd0193966229137e42273784f2f1008
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 11:51:14 -0400

    hook up webservice

--- colton/modernDatabaseCRUD/js/main.js
+++ colton/modernDatabaseCRUD/js/main.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
-		alert("Hello World");
+		alert("I'm going to get your data now!");
+		$.ajax({
+			type; 'POST',
+			url: 'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
+			success: function(json) {
+				$("#data").html(json);
+			}
+		});
 	});
-	
 	
 });
 
