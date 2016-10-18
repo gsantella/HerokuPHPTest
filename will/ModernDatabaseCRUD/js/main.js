eb346f285203e63d2bea93fff890fccf3360e841
ghill714 <wosemwegie88@southhills.edu>
2016-10-18 12:12:45 -0400

    updated syntax

--- will/ModernDatabaseCRUD/js/main.js
+++ will/ModernDatabaseCRUD/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
 		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json");
-				alert(data);
+				alert(data.title);
 				$("#data").html(data.title);
 		});
 	});
