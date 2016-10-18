027832465fe2f236c59e07a3d9b7418d9e53c808
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 12:00:38 -0400

    ajax remove

--- colton/modernDatabaseCRUD/js/main.js
+++ colton/modernDatabaseCRUD/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
 		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data){
-				$("#data").html(json);
+				$("#data").html(data);
 		});
 	});
 	
