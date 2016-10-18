40b0e50b06a64576b3bf53ba468fa2441a0802ea
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-18 12:04:51 -0400

    added toSourxe

--- allen/moderndatabasecrud/js/main.js
+++ allen/moderndatabasecrud/js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 	$('#btnLoadDataWithAJAX').click(function() {
 		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
 			function(data) { 
-				alert(data);
-				$('#data').html(data);
+				alert(data.toSource());
+				$('#data').html(data.toSource());
 		});
 	});
 });
