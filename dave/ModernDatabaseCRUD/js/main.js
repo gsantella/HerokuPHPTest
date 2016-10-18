614c8d9cf75f5d51ba74ca1c34d2ac28908acfd1
dkelz16 <dkeller16x@yahoo.com>
2016-10-18 14:37:25 -0400

    fix

--- dave/ModernDatabaseCRUD/js/main.js
+++ dave/ModernDatabaseCRUD/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 		
 		$.ajax({
 			type: "POST",
-			url "../inc/insertData.php",
+			url "inc/insertData.php",
 			data: "firstName=" + $("#txtFirstName").val(),
 			success: function()
 			{
