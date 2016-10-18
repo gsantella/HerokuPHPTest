6931ecc2d344585462cce115aef988921eabb42e
jsmith129540 <jsmith75@southhills.edu>
2016-10-18 11:19:01 -0400

    error fix

--- jared/ModernDatabaseCRUD/webform.php
+++ jared/ModernDatabaseCRUD/webform.php
@@ -3,7 +3,7 @@
 
 	<head>
 		<title>Modern Webform</title>
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 
 	<body>
