92bb065bc8afd7e0a9bb25193496e458f38b2f8c
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 12:03:30 -0400

    second alert

--- colton/modernDatabaseCRUD/js/main.js
+++ colton/modernDatabaseCRUD/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
 		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data){
+				alert(data);
 				$("#data").html(data);
 		});
 	});
