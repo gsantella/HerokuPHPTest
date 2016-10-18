a2024301a8562d4824dc0527916ac9516c9bec81
gsantella <gsantella@southhills.edu>
2016-10-18 14:12:33 -0400

    add input box

--- guido/ModernDatabaseCRUD/webform.php
+++ guido/ModernDatabaseCRUD/webform.php
@@ -9,9 +9,14 @@
 <body>
 
 <button id="btnLoadDataWithAJAX">Click me for data!</button>
-
 <div id="data">
 	Data will be updated here without a page reload!  
+</div>
+
+<input type="text" id="txtFirstName" />
+<button id="btnInsertDataWithAJAX">Add this data!</button>
+<div id="insertStatus">
+	Result of insert will show up here!
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
