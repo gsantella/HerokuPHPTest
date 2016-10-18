92f5aebaef6eff1671e2c8b0dcb448a9b136e09c
dkelz16 <dkeller16x@yahoo.com>
2016-10-18 11:56:44 -0400

    fix

--- dave/ModernDatabaseCRUD/js/main.js
+++ dave/ModernDatabaseCRUD/js/main.js
@@ -1,18 +1,14 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
 	
-	$("#btnLoadDataWithAJAX").click(function()
-	{
-		alert("Retrieving Data Now . . . ");
+	$("#btnLoadDataWithAJAX").click(function() {
+		alert("I'm going to get your data now!");
 		$.ajax({
 			type: 'POST',
 			url: 'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
-			success: function(json)
-			{
+			success: function(json) {
 				$("#data").html(json);
 			}
-			
 		});
 	});
-	
+		
 });
