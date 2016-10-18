ed9a6cfb14d3214efb56ebfcd6bc35a977140110
ghill714 <wosemwegie88@southhills.edu>
2016-10-18 12:10:46 -0400

    corrected syntax

--- will/ModernDatabaseCRUD/js/main.js
+++ will/ModernDatabaseCRUD/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
-		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json")
+		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json");
 				alert(data);
 				$("#data").html(data.title);
 		});
