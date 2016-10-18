1499aee69385834f05b76f8233842f974cf2fb76
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 12:05:16 -0400

    toSource testing

--- colton/modernDatabaseCRUD/js/main.js
+++ colton/modernDatabaseCRUD/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
 		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data){
-				alert(data);
-				$("#data").html(data);
+				alert(data.toSource());
+				$("#data").html(data.toSource);
 		});
 	});
 	
