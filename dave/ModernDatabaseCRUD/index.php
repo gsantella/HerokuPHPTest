a78b7dd7252e2244c260f48fdaec1b1f82d1ba1a
dkelz16 <dkeller16x@yahoo.com>
2016-10-18 11:13:15 -0400

    test

--- dave/ModernDatabaseCRUD/index.php
+++ dave/ModernDatabaseCRUD/index.php
@@ -6,11 +6,10 @@ include("inc/database.class.php");
 $db = new Database();
 
 //TEST THAT THE FUNCTIONS WORKS
-$db->insert("1616161616 - Dave");
-$db->insert("1616161616 - Dave");
-$db->insert("1616161616 - Dave");
-//$db->remove("1616161616 - Dave");
-
+//$db->insert("1616161616 - Dave");
+$db->remove("1616161616 - Dave");
+$db->remove("1616161616 - Dave");
+$db->remove("1616161616 - Dave");
 //CALL the getAllData function from the database object
 print_r($db->getAllData());
 
