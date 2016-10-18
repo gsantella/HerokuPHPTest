e008625bebb49e6634dbbb22b79076375c4d403f
gsantella <gsantella@southhills.edu>
2016-10-18 12:08:15 -0400

    title

--- guido/ModernDatabaseCRUD/js/main.js
+++ guido/ModernDatabaseCRUD/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
 		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data) {
-				alert(data.toSource());
-				$("#data").html(data.toSource());
+				alert(data.title);
+				$("#data").html(data.title);
 		});
 	});
 		
