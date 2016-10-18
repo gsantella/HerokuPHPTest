82c1eec4fa6aead48d179fef94db1920bcd160f4
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 12:11:31 -0400

    ajax test fixes

--- colton/modernDatabaseCRUD/js/main.js
+++ colton/modernDatabaseCRUD/js/main.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
-		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data){
-				alert(data.title());
+		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function(data) {
+				alert(data.title);
 				$("#data").html(data.title);
 		});
 	});
-	
+		
 });
-
