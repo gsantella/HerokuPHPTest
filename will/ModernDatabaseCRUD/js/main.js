ebebb4d5c0598fd11ca4131e526dad6d02642635
ghill714 <wosemwegie88@southhills.edu>
2016-10-18 12:09:12 -0400

    title

--- will/ModernDatabaseCRUD/js/main.js
+++ will/ModernDatabaseCRUD/js/main.js
@@ -1,7 +1,11 @@
-$(document).ready(function()) {
-
+$(document).ready(function() {
+	
 	$("#btnLoadDataWithAJAX").click(function() {
-		alert("Hello World!");
-	})
-
-};
+		alert("I'm going to get your data now!");
+		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json")
+				alert(data);
+				$("#data").html(data.title);
+		});
+	});
+	
+});
