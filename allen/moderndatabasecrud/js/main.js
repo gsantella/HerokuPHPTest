372c6f0620235038870269075fb4eb171586e75a
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-18 12:01:13 -0400

    added alert

--- allen/moderndatabasecrud/js/main.js
+++ allen/moderndatabasecrud/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	$('#btnLoadDataWithAJAX').click(function() {
 		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
 			function(data) { 
+				alert(data);
 				$('#data').html(data);
 		});
 	});
