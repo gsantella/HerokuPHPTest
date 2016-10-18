c073540a6b1a5543d12be0732fda0e49af2f4a06
dcarey50 <dcarey50@southhills.edu>
2016-10-18 11:56:48 -0400

    simplification

--- derek/modernDatabaseCRUD/js/main.js
+++ derek/modernDatabaseCRUD/js/main.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
-		$.ajax({
-			type: 'POST',
-			url: 'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
-			success: function(json) {
+		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json",function(data){
 				$("#data").html(json);
-			}
+			
 		});
 	});
 		
-});
+};
