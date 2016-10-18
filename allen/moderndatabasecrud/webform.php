aeb27a331db39540fe250ac3397a6efbdd57191a
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-18 11:30:50 -0400

    changed spacing

--- allen/moderndatabasecrud/webform.php
+++ allen/moderndatabasecrud/webform.php
@@ -4,15 +4,25 @@
 <head>
 	<title>Modern Webform</title>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+	<style type="text/css">
+		body {
+			padding: 100px;
+		}
+	</style>
 
 </head>
 
 <body>
 
-	<button id="btnLoadDataWithAJAX">Click Me for Data!</button>
+	<p>
+		<button id="btnLoadDataWithAJAX">Click Me for Data!</button>
+	</p>
 	
 	<div id="data">
-		Data will be updated here without a page reload - yay!
+		<p>
+			Data will be updated here without a page reload - yay!
+		</p>
 	</div>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
