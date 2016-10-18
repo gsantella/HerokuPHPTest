75f24d76ec68d73b8c8734a0da3871037914dc32
ckimberly97 <ckimberly97@southhills.edu>
2016-10-18 14:12:41 -0400

    add input clicker

--- colton/modernDatabaseCRUD/webform.php
+++ colton/modernDatabaseCRUD/webform.php
@@ -14,6 +14,12 @@
 	Data will be updated here without a page reload!  
 </div>
 
+<input type ="text" id="txtFirstName" />
+<button id="btnInsertDataWithAJAX">Add this data</button>
+<div id="insertStatus">
+	Result of insert will show up here!
+</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="js/main.js" /></script>
