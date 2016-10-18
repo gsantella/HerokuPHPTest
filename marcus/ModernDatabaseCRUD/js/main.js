6eee582196a10046348e0625a655b7e037f6f802
DarkFox216 <Darkfox216.@gmail.com>
2016-10-18 11:51:12 -0400

    added alert and ajax

--- marcus/ModernDatabaseCRUD/js/main.js
+++ marcus/ModernDatabaseCRUD/js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	
-	$("#btnLoadDataWithAJAX").click(function() {
-		alert("Hello World!");
+	$("#btnLoadDataWithAJAX").click(function() 
+		alert("I'm going to get your data now, hold on, and don't rush me!")
+		$.ajax({
+			type:'post',
+			url:'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
+			success: function(json) {
+				$("#data").html(json);
+			}
+		)};
+	
 	});
 	
 	
