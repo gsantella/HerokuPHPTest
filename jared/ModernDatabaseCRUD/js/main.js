72c76f2348c9e45ee1dea6a9e2dc958e46ce6a84
jsmith129540 <jsmith75@southhills.edu>
2016-10-18 11:57:08 -0400

    added getJSON

--- jared/ModernDatabaseCRUD/js/main.js
+++ jared/ModernDatabaseCRUD/js/main.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
-		$.ajax({
-			type: 'POST',
-			url: 'https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
-			success: function(json) {
-				$("#data").html(json);
-			}
+		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
+			success: function(data) {
+				$("#data").html(data);
+			});
 		});
 	});
 	
