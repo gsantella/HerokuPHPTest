0dc0a7f6216b65d9eb61a282f6a4f52ce717d54b
DarkFox216 <Darkfox216.@gmail.com>
2016-10-18 12:09:56 -0400

    fixed () bug

--- marcus/ModernDatabaseCRUD/js/main.js
+++ marcus/ModernDatabaseCRUD/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now, hold on, and don't rush me!");
 		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',function(data) {
-				alert(data.title());
-				$("#data").html(data.title());
+				alert(data.title);
+				$("#data").html(data.title);
 		)};
 	
 	});
