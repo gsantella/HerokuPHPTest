961bdfe4dc97123cd90c536529687381253be703
gsantella <gsantella@southhills.edu>
2016-10-18 11:42:02 -0400

    add click event handler

--- guido/ModernDatabaseCRUD/webform.php
+++ guido/ModernDatabaseCRUD/webform.php
@@ -16,6 +16,8 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+
 </body>
 
 </html>
