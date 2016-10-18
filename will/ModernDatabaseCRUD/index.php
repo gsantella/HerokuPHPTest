22f53b7a5e13390860f6ba62dcbd60cb3d519072
ghill714 <wosemwegie88@southhills.edu>
2016-10-18 11:14:39 -0400

    fixed syntax error

--- will/ModernDatabaseCRUD/index.php
+++ will/ModernDatabaseCRUD/index.php
@@ -12,7 +12,7 @@ print_r($db->getAllData());
 //Test that the insert function works
 $db->insert("allen");
 $db->insert("is");
-$db->insert("lonely"));
+$db->insert("lonely");
 $db->insert("and");
 $db->insert("need");
 $db->insert("a");
