6893a67efa04b0e600d2c79c977c7f2f5c08f919
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-18 11:58:13 -0400

    capitalized JSON

--- allen/moderndatabasecrud/js/main.js
+++ allen/moderndatabasecrud/js/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 	$('#btnLoadDataWithAJAX').click(function() {
-		$.getJson('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
+		$.getJSON('https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json',
 			function(data) { 
 				$('#data').html(data);
 		});
