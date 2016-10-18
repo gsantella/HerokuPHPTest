da424756afa7986e3d05d3a6f656a9a07c72556e
ghill714 <wosemwegie88@southhills.edu>
2016-10-18 12:22:13 -0400

    syntax error fixed

--- will/ModernDatabaseCRUD/js/main.js
+++ will/ModernDatabaseCRUD/js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 	
 	$("#btnLoadDataWithAJAX").click(function() {
 		alert("I'm going to get your data now!");
-		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json");
-				alert(data.title);
-				$("#data").html(data.title);
+		$.getJSON("https://dataclips.heroku.com/elbicfdblodrgkgwyompoodpdxae-JustFirstNames.json", function() {
+			alert(data.title);
+			$("#data").html(data.title);
 		});
 	});
 	
