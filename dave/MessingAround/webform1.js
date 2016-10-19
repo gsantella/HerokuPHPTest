ba820ce82e28b6d8493c01521037192fef255a08
dkelz16 <dkeller16x@yahoo.com>
2016-10-19 11:31:13 -0700

    d

--- dave/MessingAround/webform1.js
+++ dave/MessingAround/webform1.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	$("#signUp").click(function(){
 		alert("Account Creation Successful!");
 		$("#div").html("Account Creation Successful!");
+		$("#div").border("2px", "black");
 	});
+	
+	
 });
 	
