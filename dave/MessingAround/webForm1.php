e223da56fd1143ab1fc7bb7968d4044ec3175d07
dkelz16 <dkeller16x@yahoo.com>
2016-10-19 10:44:53 -0700

    messingAround

---
+++ dave/MessingAround/webForm1.php
@@ -0,0 +1,21 @@
+<?php
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Creating A Web Form + Processing</title>
+	</head>
+	
+	<body>
+	<h1>Sign Up</h1>
+	First Name:<input type="text" id="firstName" />
+	<br />
+	Last Name:<input type="text" id="lastName" />
+	<br />
+	<button id="signUp">Create Account</button>
+	
+	<script src="webForm1.js"></script>
+	</body>
+	
+</html>
