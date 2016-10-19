c76e19c07e2057b2ca41bfa24ddaacff9c08c387
dkelz16 <dkeller16x@yahoo.com>
2016-10-19 12:13:15 -0700

    d

---
+++ dave/MessingAround/email.php
@@ -0,0 +1,17 @@
+<?php
+
+# Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Instantiate the client.
+$mgClient = new Mailgun('key-e245d73733d3ad8b53b97992c459d66e');
+$domain = "sandbox53f0c8753fe74538aa7780fcc98bbf94.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+                  array('from'    => 'Mailgun Sandbox <postmaster@sandbox53f0c8753fe74538aa7780fcc98bbf94.mailgun.org>',
+                        'to'      => 'David Keller <dkeller16x@yahoo.com>',
+                        'subject' => 'Hello David Keller',
+                        'text'    => 'Congratulations David Keller, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
+    
--- dave/MessingAround/webForm1.php
+++ dave/MessingAround/webForm1.php
@@ -3,91 +3,20 @@
 <html>
 	<head>
 		<title>Creating A Web Form + Processing</title>
-		<LINK REL=StyleSheet HREF="main.css" TYPE="text/css" MEDIA=screen>
 	</head>
 	
 	<body>
 	<h1>Create An Account</h1>
 	
-	<div class="form">
-      
-      <ul class="tab-group">
-        <li class="tab active"><a href="#signup">Sign Up</a></li>
-        <li class="tab"><a href="#login">Log In</a></li>
-      </ul>
-      
-      <div class="tab-content">
-        <div id="signup">   
-          <h1>Sign Up for Free</h1>
-          
-          <form action="processForm.php" method="post">
-          
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" />
-            </div>
-        
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off"/>
-            </div>
-          </div>
-
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off"/>
-          </div>
-          
-          <button type="submit" class="button button-block"/>Get Started</button>
-          
-          </form>
-
-        </div>
-        
-        <div id="login">   
-          <h1>Welcome Back!</h1>
-          
-          <form action="/" method="post">
-          
-            <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off"/>
-          </div>
-          
-          <p class="forgot"><a href="#">Forgot Password?</a></p>
-          
-          <button class="button button-block"/>Log In</button>
-          
-          </form>
-
-        </div>
-        
-      </div><!-- tab-content -->
-      
-</div> <!-- /form -->
+	First Name:<input type="text" id="firstName" />
+	<br />
+	Last Name:<input type="text" id="lastName" />
+	<br />
+	<input type="submit" id="submit" action="email.php"/>
+	<button id="signUp">Create Account</button>
+	<div id="div">
+		
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script src="webform1.js"></script>
 	</body>
