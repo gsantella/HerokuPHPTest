59909d46f54ea9f351ab371719d3d3dc16ceb990
dkelz16 <dkeller16x@yahoo.com>
2016-10-19 12:36:14 -0700

    test

--- dave/MessingAround/email.php
+++ dave/MessingAround/email.php
@@ -1,17 +1,13 @@
 <?php
-echo "What the heck";
-# Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
-# Instantiate the client.
-$mgClient = new Mailgun('key-e245d73733d3ad8b53b97992c459d66e');
-$domain = "sandbox53f0c8753fe74538aa7780fcc98bbf94.mailgun.org";
-
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-                  array('from'    => 'Mailgun Sandbox <postmaster@sandbox53f0c8753fe74538aa7780fcc98bbf94.mailgun.org>',
-                        'to'      => 'David Keller <dkeller16x@yahoo.com>',
-                        'subject' => 'Hello David Keller',
-                        'text'    => 'Congratulations David Keller, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
-   echo "HEYYYYYYY";
+// Recipient
+$to = 'dkeller16x@yahoo.com';
+// Subject
+$subject = 'This is our test email';
+// Message
+$message = '<h1>Hi there.</h1><p>Thanks for testing!</p>';
+// Headers
+$headers = "From: The Sender Name <SENDER EMAIL ADDRESS>\r\n";
+$headers .= "Reply-To: REPLY-TO EMAIL ADDRESS\r\n";
+$headers .= "Content-type: text/html\r\n";
+// Send email
+mail($to, $subject, $message, $headers);
