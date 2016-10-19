9be0ac36e08098a7dae2d625843ddc1cc24bdd3e
skywalkman2210 <ahazlett24@southhills.edu>
2016-10-19 14:47:33 -0400

    added delete

--- allen/moderndatabasecrud/webform.php
+++ allen/moderndatabasecrud/webform.php
@@ -35,6 +35,14 @@
 	<div id="insertStatus">
 		Result of Insert will show up here!
 	</div>
+		
+	<br/><br/>
+
+	<input type="text" id="txtId" /><br/>
+	<button id="btnDeleteDataWithAJAX">Remove this data!</button><br/><br/>
+	<div id="deleteStatus">
+		Result of delete will show up here!
+	</div>
 	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
