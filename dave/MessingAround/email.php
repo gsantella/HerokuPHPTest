6b0708aad5abdcefecb748081ab900c6c066c171
dkelz16 <dkeller16x@yahoo.com>
2016-10-19 12:19:01 -0700

    echo

--- dave/MessingAround/email.php
+++ dave/MessingAround/email.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "What the heck";
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
@@ -14,4 +14,4 @@ $result = $mgClient->sendMessage("$domain",
                         'to'      => 'David Keller <dkeller16x@yahoo.com>',
                         'subject' => 'Hello David Keller',
                         'text'    => 'Congratulations David Keller, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
-    echo "HEYYYYYYY";
+   echo "HEYYYYYYY";
