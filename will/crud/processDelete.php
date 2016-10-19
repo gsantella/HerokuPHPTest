a42bc3b5b1ac8c7aee437c39df46209b4841f66d
ghill714 <wosemwegie88@southhills.edu>
2016-10-19 15:41:16 -0400

    added crud folder with create delete and update

---
+++ will/crud/processDelete.php
@@ -0,0 +1,22 @@
+<?php
+
+//Create Connection : Login + Select Database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+$database = new PDO($dsn);
+
+//Run a SELECT SQL Statement To Get Results
+$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+
+//To Update SQL Statement
+$stmt=$db->prepare("DELETE FROM students WHERE id=?");
+$stmt->bindValue(':id', $id, PDO::PARAM_STR);
+$stmt->execute();
+$affected_rows = $stmt -> rowCount();
+
+//Close Connection
+//Ignore For Now . . 
