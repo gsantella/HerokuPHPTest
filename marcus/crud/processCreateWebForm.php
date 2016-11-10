85da14113558886d21d006b8111cbe837cbbb641
DarkFox216 <Darkfox216.@gmail.com>
2016-11-10 15:16:00 -0500

    Updating createwebform

--- marcus/crud/processCreateWebForm.php
+++ marcus/crud/processCreateWebForm.php
@@ -1,2 +1,13 @@
 <?php
 $db->insert($_REQUEST["firstName"]);
+$stmt = $this->db->prepare("INSERT INTO students(first_name) VALUES(?);");
+		$stmtSuccess = $stmt->execute(array($data));
+		
+		if ($stmtSuccess)
+		{
+			echo "1 Record Added Successfully!";
+		}
+		else
+		{
+			echo "Insert is broken!!!!!!!";
+		}
