85760613ba411ed735fd2701d4da40026fca2dee
DarkFox216 <Darkfox216.@gmail.com>
2016-11-10 12:22:18 -0500

    testing update form

--- marcus/crud/processUpdateWebForm.php
+++ marcus/crud/processUpdateWebForm.php
@@ -1,3 +1,2 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
+<?php
+<form method="post" action="db_functions.php"></form>
