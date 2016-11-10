011174f58877527d2bd2bc17d81e81d5e01d2445
DarkFox216 <Darkfox216.@gmail.com>
2016-11-10 08:42:37 -0500

    new CRUD

---
+++ marcus/crud/processRetrieveWebForm.php
@@ -0,0 +1,16 @@
+<?php
+
+//Create Connection : Login + Select Database
+$host = "ec2-23-21-234-201.compute-1.amazonaws.com";
+$db = "ddf1ndt04b3tti";
+$user = "fpkkqmtaywxfwm";
+$pass = "dbEE8plG9zaS_QCcVzZeD7maqU";
+
+$dsn = "pgsql:dbname=$db;host=$host;user=$user;password=$pass";
+$database = new PDO($dsn);
+
+//Run a SELECT SQL Statement To Get Results
+$result = $database->query("SELECT * FROM students;")->fetchAll(PDO::FETCH_ASSOC);
+
+//Print result set
+print_r($result);
