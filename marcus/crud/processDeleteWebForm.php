0083408bc2f5dbd0d62568b860476dd226b20270
DarkFox216 <Darkfox216.@gmail.com>
2016-11-16 09:06:29 -0500

    Added text

--- marcus/crud/processDeleteWebForm.php
+++ marcus/crud/processDeleteWebForm.php
@@ -1,4 +1,15 @@
 <?php
-$stmt = $pdo->prepare("DELETE FROM students WHERE id= ?");
+ref="viewwebformcsvdatabase.php">View Data</a>
+$stmt = $pdo->prepare("DELETE FROM students WHERE id = ?");
 $stmt->execute([$cat]);
 $deleted = $stmt->fetchColumn();
+$stmtSuccess = $stmt->execute(array($data));
+		
+		if ($stmtSuccess)
+		{
+			echo "1 Record Deleted Successfully!";
+		}
+		else
+		{
+			echo "Insert is broken!!!!!!!";
+		}
